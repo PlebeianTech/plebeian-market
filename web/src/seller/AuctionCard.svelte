59aeb83132a0f2f8cc64b5cce6087f3840dd3caf
Ioan Bizău <i@ibz.me>
2022-05-03 16:05:20 +0300

    Add Twitter logo.

--- web/src/seller/AuctionCard.svelte
+++ web/src/seller/AuctionCard.svelte
@@ -77,11 +77,35 @@
                 <button class="btn mx-2" on:click={copySnippet}>Copy</button>
             {/if}
             {#if !auction.started}
-                <p>the link, tweet it, then click</p>
+                <div class="flex justify-center items-center">
+                    the link,
+                    <span class="mx-2 inline-block w-[50px] h-[50px]">
+                        <svg viewBox="328 355 335 276" xmlns="http://www.w3.org/2000/svg">
+                            <path d="
+                              M 630, 425
+                              A 195, 195 0 0 1 331, 600
+                              A 142, 142 0 0 0 428, 570
+                              A  70,  70 0 0 1 370, 523
+                              A  70,  70 0 0 0 401, 521
+                              A  70,  70 0 0 1 344, 455
+                              A  70,  70 0 0 0 372, 460
+                              A  70,  70 0 0 1 354, 370
+                              A 195, 195 0 0 0 495, 442
+                              A  67,  67 0 0 1 611, 380
+                              A 117, 117 0 0 0 654, 363
+                              A  65,  65 0 0 1 623, 401
+                              A 117, 117 0 0 0 662, 390
+                              A  65,  65 0 0 1 630, 425
+                              Z"
+                              style="fill:#3BA9EE;"/>
+                        </svg>
+                    </span>
+                    it, then click
+                </div>
                 {#if !twitterLinkCopied}
                     <button class="btn ml-2" on:click={start}>Start</button>
                 {:else}
-                    <div class="glowbutton glowbutton-start" on:click|preventDefault={start}></div>
+                    <div class="glowbutton glowbutton-start ml-2" on:click|preventDefault={start}></div>
                 {/if}
             {/if}
         </div>
