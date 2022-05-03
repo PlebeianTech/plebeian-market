143a1a361ba3b33120315d5781ae823bd998197a
Ioan Bizău <i@ibz.me>
2022-05-03 21:15:58 +0300

    Button to open Twitter.

--- web/src/seller/AuctionCard.svelte
+++ web/src/seller/AuctionCard.svelte
@@ -5,6 +5,7 @@
 
     export let auction = null;
     let twitterLinkCopied = false;
+    let twitterOpened = false;
 
     export let onEdit = (auction) => {};
 
@@ -14,6 +15,13 @@
 
     function copySnippet() {
         navigator.clipboard.writeText(`https://plebeian.market/app/buyer#plebeian-auction-${auction.key}`).then(() => { twitterLinkCopied = true; alert("URL copied!"); });
+    }
+
+    function openTwitter() {
+        twitterOpened = true;
+        let url = encodeURIComponent(`https://plebeian.market/app/buyer#plebeian-auction-${auction.key}`);
+        let text = encodeURIComponent(`I am auctioning for sats: ${auction.title}`);
+        window.open(`https://twitter.com/intent/tweet?url=${url}&text=${text}`, '_blank');
     }
 
     function view() {
@@ -68,46 +76,25 @@
             {/if}
         </div>
     </div>
+    {#if !auction.started}
     <div class="mt-2">
         <div class="py-5 w-full flex items-center justify-center bg-gray-800 rounded">
-            <p class="text-zinc-300 text-2xl mr-2">Twitter</p>
-            {#if !twitterLinkCopied && !auction.started}
+            {#if !twitterLinkCopied && !twitterOpened}
                 <div class="glowbutton glowbutton-copy mx-2" on:click|preventDefault={copySnippet}></div>
             {:else}
                 <button class="btn mx-2" on:click={copySnippet}>Copy</button>
             {/if}
-            {#if !auction.started}
-                <div class="flex justify-center items-center">
-                    the link,
-                    <span class="mx-2 inline-block w-[50px] h-[50px]">
-                        <svg viewBox="328 355 335 276" xmlns="http://www.w3.org/2000/svg">
-                            <path d="
-                              M 630, 425
-                              A 195, 195 0 0 1 331, 600
-                              A 142, 142 0 0 0 428, 570
-                              A  70,  70 0 0 1 370, 523
-                              A  70,  70 0 0 0 401, 521
-                              A  70,  70 0 0 1 344, 455
-                              A  70,  70 0 0 0 372, 460
-                              A  70,  70 0 0 1 354, 370
-                              A 195, 195 0 0 0 495, 442
-                              A  67,  67 0 0 1 611, 380
-                              A 117, 117 0 0 0 654, 363
-                              A  65,  65 0 0 1 623, 401
-                              A 117, 117 0 0 0 662, 390
-                              A  65,  65 0 0 1 630, 425
-                              Z"
-                              style="fill:#3BA9EE;"/>
-                        </svg>
-                    </span>
-                    it, then click
-                </div>
-                {#if !twitterLinkCopied}
-                    <button class="btn ml-2" on:click={start}>Start</button>
-                {:else}
-                    <div class="glowbutton glowbutton-start ml-2" on:click|preventDefault={start}></div>
-                {/if}
+            {#if twitterLinkCopied && !twitterOpened}
+                <div class="glowbutton glowbutton-tweet mx-2" on:click|preventDefault={openTwitter}></div>
+            {:else}
+                <button class="btn mx-2" on:click={openTwitter}>Tweet</button>
+            {/if}
+            {#if twitterLinkCopied && twitterOpened}
+                <div class="glowbutton glowbutton-start ml-2" on:click|preventDefault={start}></div>
+            {:else}
+                <button class="btn mx-2" on:click={start}>Start</button>
             {/if}
         </div>
     </div>
+    {/if}
 </div>
