6e3159f920d756a38eb45a22d379169a40954814
Ioan Bizău <i@ibz.me>
2022-05-04 15:57:54 +0300

    Switch to Daisy navbar.

---
+++ web/src/Navbar.svelte
@@ -0,0 +1,27 @@
+<script>
+    import { token } from "./stores.js";
+
+    export let onSelect = (key) => {};
+</script>
+
+<div class="navbar bg-base-100">
+    <div class="flex-1">
+        <a href={null} on:click|preventDefault={() => onSelect('app')} class="btn btn-ghost normal-case text-xl">Home</a>
+        <a href={null} on:click|preventDefault={() => onSelect('about')} class="btn btn-ghost normal-case text-xl">About</a>
+    </div>
+    <div class="flex-none gap-2">
+        {#if $token}
+            <div class="dropdown dropdown-end">
+                <label for={null} tabindex="0" class="btn btn-ghost btn-circle avatar">
+                    <div class="w-10 rounded-full">
+                        <img src="https://api.lorem.space/image/face?hash=33791" alt="Avatar" />
+                    </div>
+                </label>
+                <ul tabindex="0" class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
+                    <li><a href={null} on:click|preventDefault={() => onSelect('profile')}>Profile</a></li>
+                    <li><a href={null} on:click|preventDefault={() => onSelect('logout')}>Logout</a></li>
+                </ul>
+            </div>
+        {/if}
+    </div>
+  </div>
--- web/src/seller/App.svelte
+++ web/src/seller/App.svelte
@@ -6,15 +6,20 @@
     import Login from "../Login.svelte";
     import About from "./About.svelte";
     import Auctions from "./Auctions.svelte";
+    import Navbar from "../Navbar.svelte";
     import Profile from "../Profile.svelte";
 
     let isLoading = false;
 
     let selected = 'app';
 
-    function logout() {
-        token.set(null);
-        selected = 'app';
+    function onSelect(key) {
+        if (key === 'logout') {
+            token.set(null);
+            selected = 'app';
+        } else {
+            selected = key;
+        }
     }
 
     const unsubscribe = token.subscribe(tokenValue => {
@@ -39,42 +44,7 @@
 </script>
 
 <div class="flex flex-col">
-    <nav class="bg-white dark:bg-slate-900 px-2 sm:px-4 py-2.5 rounded">
-        <div class="container flex flex-wrap justify-between items-center mx-auto">
-            <span class="flex items-center">
-                <img src="/static/images/logo.jpg" class="mr-3 h-6 sm:h-9 rounded" alt="Plebeian Logo" />
-                <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Plebeian Market</span>
-            </span>
-            <span class="w-full md:block md:w-auto">
-                <span class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
-                    <span>
-                        <a href={null} on:click={() => selected = 'app'} class="h-full block pr-4 pl-3 flex justify-center items-center {selected === 'app' ? 'selected-menu-item' : 'menu-item'}" aria-current="page">Home</a>
-                    </span>
-                    <span>
-                        <a href={null} on:click={() => selected = 'about'} class="h-full block pr-4 pl-3 flex justify-center items-center {selected === 'about' ? 'selected-menu-item' : 'menu-item'}">About</a>
-                    </span>
-                    <span class="flex items-center">
-                        {#if $token}
-                        <span class="dropdown dropdown-end">
-                            <button tabindex="0" type="button" class="btn flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600">
-                                <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-                                <svg class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                            </button>
-                                <ul tabindex="0" class="p-2 dropdown-content menu rounded-box shadow bg-base-100 w-52">
-                                    <li>
-                                        <a href={null} on:click|preventDefault={() => selected = 'profile'} class="cursor-pointer block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Profile</a>
-                                    </li>
-                                    <li>
-                                        <a href={null} on:click|preventDefault={logout} class="cursor-pointer block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Log out</a>
-                                    </li>
-                                </ul>
-                        </span>
-                        {/if}
-                    </span>
-                </span>
-            </span>
-        </div>
-    </nav>
+    <Navbar onSelect={onSelect} />
 
     {#if selected === 'about'}
         <About />
