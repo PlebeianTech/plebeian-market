ffb547def94755ecbd2324202cf7eb4a69e25b7b
Henry Romp <151henry151@gmail.com>
2022-06-07 17:03:05 -0400

    Carousel for mobile and gallery for desktop.

--- web/src/lib/components/Carousel.svelte
+++ web/src/lib/components/Carousel.svelte
@@ -4,7 +4,7 @@
     export let photos: Media[];
 </script>
 
-<div class="visible lg:invisible mt-4 carousel carousel-center space-x-4">
+<div class="lg:hidden mt-4 carousel carousel-center space-x-4">
     {#each photos as photo, i}
         <div id="{photo.twitter_media_key}" class="carousel-item">
             <img class="h-96 w-48 lg:w-96 object-scale-down" src={photo.url} alt="Auctioned object" />
