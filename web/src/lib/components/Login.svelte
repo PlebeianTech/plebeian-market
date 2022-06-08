678f118782b35a2fa6787c3ce42d2187692c75f4
Henry Romp <151henry151@gmail.com>
2022-06-08 14:43:26 -0400

    Add links to compatible wallets for login.

--- web/src/lib/components/Login.svelte
+++ web/src/lib/components/Login.svelte
@@ -42,7 +42,7 @@
 <div class="pt-10 flex justify-center items-center">
     {#if qr}
         <div>
-            <p class="mb-4">Scan with a <a class="link" href="https://github.com/fiatjaf/lnurl-rfc#lnurl-documents" target="_blank">compatible</a> wallet to enter the marketplace.</p>
+            <p class="mb-4 text-center">Scan with <a class="link" href="https://breez.technology/">Breez,</a> <a class="link" href="https://phoenix.acinq.co/">Phoenix,</a> <a class="link" href="https://thunderhub.io/">Thunderhub,</a> <a class="link" href="https://zeusln.app/">Zeus,</a> <a class="link" href="https://getalby.com/">Alby,</a> or any <a class="link" href="https://github.com/fiatjaf/lnurl-rfc#lnurl-documents" target="_blank">LNurl compatible</a> wallet to enter the marketplace.</p>
 
             <QR bind:qr={qr} bind:lnurl={lnurl} />
         </div>
