080297fb11adb3c8183e702f33b98da4101eb8e8
Henry Romp <151henry151@gmail.com>
2022-06-09 01:31:47 -0400

    Increase content width on about page.

--- web/src/routes/about.svelte
+++ web/src/routes/about.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <div class="flex justify-center items-center">
-    <div class="mt-10 ml-4 w-2/4">
+    <div class="mt-10 ml-4 w-3/4 lg:w-2/4">
         <p class="mt-2">This is an MVP and an incidental idea on the way to a fully sovereign version... it's the first piece of a much bigger vision of decentralised sovereign commerce.</p>
         <p class="mt-2">100% value4value business model... 100% donations to this site go into open source bitcoin projects.</p>
         <p class="mt-2">Sellers decide how much of their sale they want to donate. The buyer pays it in settlement of the auction they won or the product they bought.</p>
