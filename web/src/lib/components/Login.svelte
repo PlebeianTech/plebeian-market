befad84d89ffda42e09a66ab0ae7335ba0b3adab
Henry Romp <151henry151@gmail.com>
2022-06-09 00:05:19 -0400

    Adjust login phrasing for web-based lightning wallets

--- web/src/lib/components/Login.svelte
+++ web/src/lib/components/Login.svelte
@@ -42,7 +42,14 @@
 <div class="pt-10 flex justify-center items-center">
     {#if qr}
         <div>
-            <p class="mb-4 text-center">Scan with <a class="link" href="https://breez.technology/">Breez,</a> <a class="link" href="https://phoenix.acinq.co/">Phoenix,</a> <a class="link" href="https://thunderhub.io/">Thunderhub,</a> <a class="link" href="https://zeusln.app/">Zeus,</a> <a class="link" href="https://getalby.com/">Alby,</a> or any <a class="link" href="https://github.com/fiatjaf/lnurl-rfc#lnurl-documents" target="_blank">LNurl compatible</a> wallet to enter the marketplace.</p>
+            <p class="mb-4 text-center">Scan with <a class="link" href="https://breez.technology/">Breez,</a>
+                <a class="link" href="https://phoenix.acinq.co/">Phoenix,</a>
+                <a class="link" href="https://zeusln.app/">Zeus,</a>
+                </p>
+                <p class="mb-4 text-center">or use <a class="link" href="https://getalby.com/">Alby,</a>
+                    <a class="link" href="https://thunderhub.io/">Thunderhub</a>
+                    or any <a class="link" href="https://github.com/fiatjaf/lnurl-rfc#lnurl-documents" target="_blank">
+                        LNurl compatible wallet </a> to enter the marketplace.</p>
 
             <QR bind:qr={qr} bind:lnurl={lnurl} />
         </div>
