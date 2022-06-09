206dd7316d755e81ee41c67dc1c522593bd70afc
Henry Romp <151henry151@gmail.com>
2022-06-09 02:46:33 -0400

    Rough draft of changes to home page for issue #11

--- web/src/routes/index.svelte
+++ web/src/routes/index.svelte
@@ -5,13 +5,14 @@
 <script>
 </script>
 
-<div class="mt-4 md:columns-4">
-    <div class="flex"><div class="text-9xl">1.</div><div class="h-full text-4xl">Basic details</div></div>
-    <div class="flex"><div class="text-9xl">2.</div><div class="h-full text-4xl">Tweet it with photos</div></div>
-    <div class="flex"><div class="text-9xl">3.</div><div class="h-full text-4xl">Click start</div></div>
-    <div class="flex"><div class="text-9xl">4.</div><div class="h-full text-4xl">Collect sats</div></div>
+<div class="mt-4 md:columns-5">
+    <div class="invisible flex"><div class="h-full text-4xl">Placeholder Text</div></div>
+    <div class="flex"><div class="text-9xl">1.</div><div class="h-full text-4xl">Tweet with Photos</div></div>
+    <div class="flex"><div class="text-9xl">2.</div><div class="h-full text-4xl">Click Start</div></div>
+    <div class="flex"><div class="text-9xl">3.</div><div class="h-full text-4xl">Collect Sats</div></div>
 </div>
-
+<p class="line-1 anim-typewriter_1">Sell anything... ... and get paid in sats.</p>
+<p class="line-2 anim-typewriter_2">1 hour of your time, bitcoin art, your furniture, an ASIC miner, Baklava, your next book.</p>
 <div class="pt-10 flex justify-center items-center">
     <a class="btn" href="/login">Enter the market!</a>
 </div>
