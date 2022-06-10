e3955e8c234ac1bcd480df43d0a242bd6f23fa39
Henry Romp <151henry151@gmail.com>
2022-06-10 13:20:24 -0400

    Adjust display of index page for mobile

--- web/src/routes/index.svelte
+++ web/src/routes/index.svelte
@@ -7,28 +7,28 @@
 </script>
 <div class="flex justify-center">
 <div class="mt-4 md:columns-3 w-3/5">
-    <div class="flex"><div class="text-9xl">1.</div><div class="h-full text-4xl">Tweet Photos</div></div>
-    <div class="flex"><div class="text-9xl">2.</div><div class="h-full text-4xl">Click Start</div></div>
-    <div class="flex"><div class="text-9xl">3.</div><div class="h-full text-4xl">Collect Sats</div></div>
+    <div class="flex"><div class="text-7xl lg:text-9xl">1.</div><div class="h-full text-2xl lg:text-4xl">Tweet Photos</div></div>
+    <div class="flex"><div class="text-7xl lg:text-9xl">2.</div><div class="h-full text-2xl lg:text-4xl">Click Start</div></div>
+    <div class="flex"><div class="text-7xl lg:text-9xl">3.</div><div class="h-full text-2xl lg:text-4xl">Collect Sats</div></div>
 </div>
 </div>
 <div class="flex justify-center">
-<div class="mt-12 columns-3 w-3/4">
+<div class="mt-12 columns-1 lg:columns-3 lg:w-3/4">
     <span>
-        <p class="flex-row text-3xl text-right">Sell anything...</p>
+        <p class="flex-row text-xs md:text-xl lg:text-3xl lg:text-right">Sell anything...</p>
     </span>
     <span class="flex bg-black/30">
 <Typewriter interval={[50, 60, 95, 80]} cursor='255,255,255,30' loop>
-<p class="text-3xl">1 hour of your time</p>
-<p class="text-3xl">bitcoin art</p>
-<p class="text-3xl">your furniture</p>
-<p class="text-3xl">an ASIC miner</p>
-<p class="text-3xl">Baklava</p>
-<p class="text-3xl">your next book</p>
+<p class="text-xs md:text-xl lg:text-3xl">1 hour of your time</p>
+<p class="text-xs md:text-xl lg:text-3xl">bitcoin art</p>
+<p class="text-xs md:text-xl lg:text-3xl">your furniture</p>
+<p class="text-xs md:text-xl lg:text-3xl">an ASIC miner</p>
+<p class="text-xs md:text-xl lg:text-3xl">Baklava</p>
+<p class="text-xs md:text-xl lg:text-3xl">your next book</p>
 </Typewriter>
     </span>
     <span>
-        <p class="text-3xl text-left">...and get paid in sats.</p>
+        <p class="text-xs md:text-xl lg:text-3xl text-left">...and get paid in sats.</p>
     </span>
 </div>
 </div>
