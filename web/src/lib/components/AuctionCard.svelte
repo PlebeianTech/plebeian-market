9fcf3db8e648979c47c56eae3fb1088fb32d5bab
Ioan Bizău <i@ibz.me>
2022-06-11 05:47:01 +0300

    Turn auction card into a Daisy card.

--- web/src/lib/components/AuctionCard.svelte
+++ web/src/lib/components/AuctionCard.svelte
@@ -77,116 +77,109 @@
     onMount(async () => { confirmation = null; });
 </script>
 
-<div class="card bg-base-300 max-w-full p-4 rounded overflow-hidden shadow-lg my-3">
-    <div class="text-center">
-        <h3 class="text-4xl">{auction.title}</h3>
-        <span class=" font-mono">
+<div class="glowbox card lg:card-side bg-base-100 max-w-full p-4 overflow-hidden shadow-xl my-3">
+    <figure>
+        {#each auction.media as photo, i}
+            {#if i === 0}
+                <img src={photo.url} alt="Auctioned object" />
+            {/if}
+        {/each}
+    </figure>
+    <div class="card-body">
+        <h2 class="card-title">
+            {auction.title}
             {#if auction.started && !auction.ended}
                 (running)
             {:else if auction.ended}
                 (ended)
             {/if}
-        </span>
-    </div>
-    {#if auction.start_date && auction.end_date}
-    <div class="lg:flex lg:mt-2 text-xl">
-        <div>
-            <span class="visible lg:invisible lg:w-0 inline-block">From:</span>
-            <DateFormatter date={auction.start_date} />
-        </div>
-        <div class="invisible h-0 lg:h-auto lg:visible">&nbsp;-&nbsp;</div>
-        <div>
-            <span class="visible lg:invisible lg:w-0 inline-block">To:</span>
-            <DateFormatter date={auction.end_date} />
-        </div>
-    </div>
-    {/if}
-    <div class="mt-2 text-2xl">
-        <div>
-            {#if !auction.started}
-                <span>{auction.duration_str} /</span>
-            {/if}
-            <span>Starts at: {auction.starting_bid} /</span>
-            <span>Reserve: {auction.reserve_bid}</span>
-            {#if auction.started}
-                <div class="lg:float-right">Bids: {auction.bids.length}</div>
-            {/if}
-        </div>
-    </div>
-    {#if confirmation}
-        <div class="mt-2 py-2 lg:py-5">
-            <Confirmation onContinue={confirmation.onContinue} onCancel={() => confirmation = null} />
-        </div>
-    {:else}
-        <div class="mt-2 float-root">
-            <div class="py-5 float-left">
-                <div class="lg:flex">
-                    <div>
-                        {#each auction.media as photo, i}
-                            {#if i === 0}
-                                <div id="{photo.twitter_media_key}" class="w-36 rounded">
-                                    <img src={photo.url} class="rounded" alt="Auctioned object" />
-                                </div>
-                            {/if}
-                        {/each}
-                    </div>
-                    <div class="mt-4 lg:mt-0">
-                        {#if auction.started && !auction.ended}
-                            <div class="ml-4 mt-2">
-                                <Countdown untilDate={auction.end_date} />
-                            </div>
-                        {/if}
-                    </div>
+        </h2>
+        {#if auction.start_date && auction.end_date}
+            <div class="lg:flex lg:mt-2">
+                <div>
+                    <span class="visible lg:invisible lg:w-0 inline-block">From:</span>
+                    <DateFormatter date={auction.start_date} />
+                </div>
+                <div>
+                    <span class="visible lg:invisible lg:w-0 inline-block">To:</span>
+                    <DateFormatter date={auction.end_date} />
                 </div>
             </div>
-            {#if !auction.started}
-                <div class="mt-7 lg:mt-0 py-5 float-right">
-                    <button class="btn mx-1" on:click={() => onEdit(auction)}>Edit</button>
-                    <button class="btn mx-1" on:click={del}>Delete</button>
-                </div>
-            {:else if auctionViewed || auction.ended}
-                <div class="py-5 float-right">
-                    <button class="btn mx-1" on:click={view}>View</button>
-                </div>
-            {/if}
+        {/if}
+        <div class="mt-2">
+            <div>
+                {#if !auction.started}
+                    <span>{auction.duration_str} /</span>
+                {/if}
+                <span>Start: {auction.starting_bid} /</span>
+                <span>Reserve: {auction.reserve_bid}</span>
+                {#if auction.started}
+                    <div class="lg:float-right">Bids: {auction.bids.length}</div>
+                {/if}
+            </div>
         </div>
-    {/if}
-    {#if !auction.ended && !auctionViewed}
-    <div class="mt-2">
-        <p class="text-center">
-            {#if !auctionTweeted && !auction.started}
-            Create your tweet and don't forget to attach some pictures
-            {:else if auctionTweeted && !auction.started}
-            Start your auction
-            {:else if auction.started}
-            View your auction
+            {#if auction.started && !auction.ended}
+                <div class="mt-2 float-root">
+                    <div class="py-5 float-left">
+                        <div class="lg:flex">
+                            <div class="mt-4 lg:mt-0">
+                                <div class="ml-4 mt-2">
+                                    <Countdown untilDate={auction.end_date} />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             {/if}
-        </p>
-        <div class="pt-5 mb-5 w-full flex items-center justify-center rounded">
-            <ul class="steps steps-vertical lg:steps-horizontal">
-                <li class="step lg:mb-5 lg:ml-5" class:step-primary={true}>
-                    {#if !auctionTweeted && !auction.started}
-                        <div class="glowbutton glowbutton-tweet mx-2" on:click|preventDefault={openTwitter}></div>
-                    {:else}
-                        <button class="btn mx-2" on:click={openTwitter}>Tweet</button>
-                    {/if}
-                </li>
-                <li class="step lg:mb-5" class:step-primary={auctionTweeted || auction.started}>
-                    {#if auctionTweeted && !auction.started && !starting}
-                        <div class="glowbutton glowbutton-start mx-2" on:click|preventDefault={start}></div>
-                    {:else}
-                        <button class="btn btn-disabled mx-2">Start</button>
-                    {/if}
-                </li>
-                <li class="step lg:mb-5 lg:mr-5" class:step-primary={auction.started}>
-                    {#if auction.started}
-                        <div class="glowbutton glowbutton-view ml-2" on:click|preventDefault={view}></div>
-                    {:else}
-                        <button class="btn mx-2" on:click={view}>View</button>
-                    {/if}
-                </li>
-            </ul>
+        {#if !auction.ended && !auctionViewed}
+        <div class="mt-2">
+            <p class="text-center">
+                {#if !auctionTweeted && !auction.started}
+                Create your tweet and don't forget to attach some pictures
+                {:else if auctionTweeted && !auction.started}
+                Start your auction
+                {:else if auction.started}
+                View your auction
+                {/if}
+            </p>
+            <div class="pt-5 mb-5 w-full flex items-center justify-center rounded">
+                <ul class:lg:steps-horizontal={!auction.started} class="steps steps-vertical">
+                    <li class="step" class:step-primary={true} class:lg:mb-5={!auction.started} class:lg:ml-5={!auction.started}>
+                        {#if !auctionTweeted && !auction.started}
+                            <div class="glowbutton glowbutton-tweet mx-2" on:click|preventDefault={openTwitter}></div>
+                        {:else}
+                            <button class="btn mx-2" on:click={openTwitter}>Tweet</button>
+                        {/if}
+                    </li>
+                    <li class="step" class:lg:mb-5={!auction.started} class:step-primary={auctionTweeted || auction.started}>
+                        {#if auctionTweeted && !auction.started && !starting}
+                            <div class="glowbutton glowbutton-start mx-2" on:click|preventDefault={start}></div>
+                        {:else}
+                            <button class="btn btn-disabled mx-2">Start</button>
+                        {/if}
+                    </li>
+                    <li class="step" class:lg:mb-5={!auction.started} class:lg:mr-5={!auction.started} class:step-primary={auction.started}>
+                        {#if auction.started}
+                            <div class="glowbutton glowbutton-view ml-2" on:click|preventDefault={view}></div>
+                        {:else}
+                            <button class="btn mx-2" on:click={view}>View</button>
+                        {/if}
+                    </li>
+                </ul>
+            </div>
+        </div>
+        {/if}
+        <div class="card-actions justify-end">
+            {#if confirmation}
+                    <Confirmation onContinue={confirmation.onContinue} onCancel={() => confirmation = null} />
+            {:else}
+                {#if !auction.started}
+                        <button class="btn mx-1" on:click={() => onEdit(auction)}>Edit</button>
+                        <button class="btn mx-1" on:click={del}>Delete</button>
+                {:else if auctionViewed || auction.ended}
+                        <button class="btn mx-1" on:click={view}>View</button>
+                {/if}
+            {/if}
         </div>
     </div>
-    {/if}
 </div>
--- web/src/lib/components/Confirmation.svelte
+++ web/src/lib/components/Confirmation.svelte
@@ -11,16 +11,11 @@
     }
 </script>
 
-<div class="bg-warn flex items-center justify-center w-full">
-    <div class="form-control w-full">
-        <label class="label cursor-pointer">
-            <div class="flex lg:block items-center label-text w-full text-right text-error mr-2">
-                <span>I am sure!</span>
-                <span class="inline-block invisible w-0 lg:visible lg:w-auto">&nbsp;I want to delete this auction!</span>
-            </div>
-            <input type="checkbox" class="toggle toggle-secondary toggle-lg" bind:value={confirm} />
-        </label>
+<label class="label cursor-pointer">
+    <div class="items-center label-text w-full text-right text-error mr-2">
+        YES!
     </div>
-    <button class="btn mx-2" on:click|preventDefault={onCancel}>Cancel</button>
-    <button class="btn btn-error mx-2" on:click|preventDefault={check}>Delete</button>
-</div>
+    <input type="checkbox" class="toggle toggle-secondary toggle-lg" bind:value={confirm} />
+</label>
+<button class="btn mx-2" on:click|preventDefault={onCancel}>Cancel</button>
+<button class="btn btn-error mx-2" on:click|preventDefault={check}>Delete</button>
