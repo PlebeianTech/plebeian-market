70d83d04df63046dcaf3a8eab597dc592b1d51d6
Ioan Bizău <i@ibz.me>
2022-06-13 16:56:19 +0300

    Handshake tweaks.

--- web/src/lib/components/Handshake.svelte
+++ web/src/lib/components/Handshake.svelte
@@ -6,26 +6,26 @@
 </script>
 
 <div class="bg-base-200 flex justify-center items-center max-w-full rounded py-2">
-    <div class="text-center w-1/3">
+    <div class="text-center w-1/6">
         <div class="avatar">
-            <div class="w-24 rounded-full">
+            <div class="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
                 <img src={leftProfileImageUrl} alt="Avatar" />
             </div>
         </div>
-        <p class="text-2xl ml-5">@{leftUsername}</p>
+        <p class="text-center text-xl">@{leftUsername}</p>
     </div>
     <div class="text-center text-6xl w-1/8">
         &#129309;
     </div>
-    <div class="text-center w-1/3">
+    <div class="text-center w-1/6">
         <div class="avatar">
-            <div class="w-24 rounded-full">
+            <div class="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
                 <a href="https://twitter.com/{rightUsername}" target="_blank">
                     <img src={rightProfileImageUrl} alt="Avatar" />
                 </a>
             </div>
         </div>
-        <p class="text-2xl ml-5">
+        <p class="text-center text-xl">
             <a href="https://twitter.com/{rightUsername}" target="_blank">
                 @{rightUsername}
             </a>
