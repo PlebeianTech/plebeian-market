041751486b0e9321b7523cc255ca863849a26627
Henry Romp <henry@romp.network>
2022-06-15 14:20:53 -0400

    Adjust text color to neon pink.

--- web/src/routes/index.svelte
+++ web/src/routes/index.svelte
@@ -18,7 +18,7 @@
     </span>
     <span class="flex justify-center">
 <Typewriter interval={[50, 60, 95, 80]} cursor='255,255,255,30' loop>
-        <span class="text-pink-500 justify-center text-s md:text-xl lg:text-3xl">
+        <span class="text-neon-pink justify-center text-s md:text-xl lg:text-3xl">
             <p>1 hour of your time</p>
             <p>bitcoin art</p>
             <p>your furniture</p>
