4b8d026eeeff474507035a80a6d9a64d9ac77b24
Henry Romp <henry@romp.network>
2022-06-15 14:21:12 -0400

    Add neon pink to theme colors

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,7 +1,11 @@
 module.exports = {
   content: ['./src/lib/components/*.svelte', './src/routes/*.svelte'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'neon-pink': '#ff00ff',
+      }
+    },
   },
   plugins: [require("daisyui")],
   daisyui: {
