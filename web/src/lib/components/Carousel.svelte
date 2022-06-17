5306b2880f12bf9b1387515014a1ab5cc3add8ad
Henry Romp <henry@romp.network>
2022-06-17 01:01:16 -0400

    Don't clip sides off images on mobile carousel.

--- web/src/lib/components/Carousel.svelte
+++ web/src/lib/components/Carousel.svelte
@@ -7,7 +7,7 @@
 <div class="mt-4 carousel carousel-center space-x-4">
     {#each photos as photo, i}
         <div id="{photo.twitter_media_key}" class="carousel-item">
-            <img class="h-96 w-48 object-cover rounded-md" src={photo.url} alt="Auctioned object" />
+            <img class="self-center h-fit w-48 object-scale-down rounded-md" src={photo.url} alt="Auctioned object" />
         </div>
     {/each}
 </div>
