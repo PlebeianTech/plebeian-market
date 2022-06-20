7c08431500e8da1a686c8629ca64f4f19572f16c
Ioan Bizău <i@ibz.me>
2022-06-20 07:53:05 +0200

    Staging environment.

--- scripts/do.sh
+++ scripts/do.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-USAGE="$0 (test|dev|prod) ..."
+USAGE="$0 (test|dev|staging|prod) ..."
 
 ENV=$1
 shift
@@ -9,8 +9,10 @@ if [ "$ENV" = "test" ]; then
     docker-compose -f docker-compose.yml -f docker-compose.dev.yml -f docker-compose.test.yml $@
 elif [ "$ENV" = "dev" ]; then
     docker-compose -f docker-compose.yml -f docker-compose.dev.yml $@
+elif [ "$ENV" = "staging" ]; then
+    docker-compose -f docker-compose.yml -f docker-compose.staging.yml $@
 elif [ "$ENV" = "prod" ]; then
-    docker-compose -f docker-compose.yml -f docker-compose.prod.yml $@
+    docker-compose -f docker-compose.yml -f docker-compose.staging.yml -f docker-compose.prod.yml $@
 else
     echo $USAGE
     exit 1
