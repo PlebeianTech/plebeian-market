5cfbf23ba3e2185b1c84e1cd24fc784b34b798d7
Henry Romp <henry@romp.network>
2022-06-20 16:51:40 -0400

    Make text bigger and more space between paragraphs. Add self.

--- web/src/routes/about.svelte
+++ web/src/routes/about.svelte
@@ -5,12 +5,12 @@
 <script>
 </script>
 
-<div class="flex justify-center items-center">
-    <div class="mt-10 ml-4 w-3/4 lg:w-2/4">
-        <p class="mt-2">This is an MVP and an incidental idea on the way to a fully sovereign version... it's the first piece of a much bigger vision of decentralised sovereign commerce.</p>
-        <p class="mt-2">100% value4value business model... 100% donations to this site go into open source bitcoin projects.</p>
-        <p class="mt-2">Sellers decide how much of their sale they want to donate. The buyer pays it in settlement of the auction they won or the product they bought.</p>
-        <p class="mt-2">This site is FOSS and will initially be released under the GNU General Public License (GPL). Once we have completed the MVPs for the other components of the ecosystem we will transition to a more permissive license, most likely MIT.</p>
-        <p class="mt-2">We are @Chiefmonkey and @ibz.</p>
+<div class="flex justify-center items-center md:mb-20">
+    <div class="mt-10 ml-4 w-3/4 md:text-3xl">
+        <p class="mt-2 md:mt-10">This is an MVP and an incidental idea on the way to a fully sovereign version... it's the first piece of a much bigger vision of decentralised sovereign commerce.</p>
+        <p class="mt-2 md:mt-10">100% value4value business model... 100% donations to this site go into open source bitcoin projects.</p>
+        <p class="mt-2 md:mt-10">Sellers decide how much of their sale they want to donate. The buyer pays it in settlement of the auction they won or the product they bought.</p>
+        <p class="mt-2 md:mt-10">This site is FOSS and will initially be released under the GNU General Public License (GPL). Once we have completed the MVPs for the other components of the ecosystem we will transition to a more permissive license, most likely MIT.</p>
+        <p class="mt-2 md:mt-10">We are @Chiefmonkey, @ibz, and @RompHenry.</p>
     </div>
 </div>
