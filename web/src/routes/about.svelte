302d66311e19e2cf351b3ec4414fa608de07748d
Ioan Bizău <i@ibz.me>
2022-06-21 17:31:19 +0300

    Add Bitko!

--- web/src/routes/about.svelte
+++ web/src/routes/about.svelte
@@ -11,6 +11,6 @@
         <p class="mt-2 md:mt-10">100% value4value business model... 100% donations to this site go into open source bitcoin projects.</p>
         <p class="mt-2 md:mt-10">Sellers decide how much of their sale they want to donate. The buyer pays it in settlement of the auction they won or the product they bought.</p>
         <p class="mt-2 md:mt-10">This site is FOSS and will initially be released under the GNU General Public License (GPL). Once we have completed the MVPs for the other components of the ecosystem we will transition to a more permissive license, most likely MIT.</p>
-        <p class="mt-2 md:mt-10">We are @Chiefmonkey, @ibz, and @RompHenry.</p>
+        <p class="mt-2 md:mt-10">We are @Chiefmonkey, @ibz, @RompHenry and @BitkoYinowsky.</p>
     </div>
 </div>
