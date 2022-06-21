fc28beea888ee2949022dcba79aee3a6509ce164
Ioan Bizău <i@ibz.me>
2022-06-21 17:08:28 +0300

    Add Telegram link.

--- web/src/lib/components/Navbar.svelte
+++ web/src/lib/components/Navbar.svelte
@@ -68,6 +68,7 @@
                 <ul tabindex="0" class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
                     <li><a href={null} on:click|preventDefault={() => goto("/auctions")}>My auctions</a></li>
                     <li><label for="profile-modal" on:click|preventDefault={profile.show} class="modal-button">Profile</label></li>
+                    <li><a href="https://t.me/PlebeianMarket" target="_blank">Telegram group</a></li>
                     <li><a href={null} on:click|preventDefault={() => { token.set(null); localStorage.removeItem('token'); goto("/"); }}>Logout</a></li>
                 </ul>
             </div>
