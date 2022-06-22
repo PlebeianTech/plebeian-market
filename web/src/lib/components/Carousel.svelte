11762236232942efd7e79596643bd5de9d6bc8cc
Henry Romp <151henry151@gmail.com>
2022-06-22 18:59:51 -0400

    Fix space above and below carousel images on mobile screens.

--- web/src/lib/components/Carousel.svelte
+++ web/src/lib/components/Carousel.svelte
@@ -4,7 +4,7 @@
     export let photos: Media[];
 </script>
 
-<div class="mt-4 carousel carousel-center space-x-4">
+<div class="max-h-48 mt-4 carousel carousel-center space-x-4">
     {#each photos as photo, i}
         <div id="{photo.twitter_media_key}" class="carousel-item">
             <img class="self-center h-fit w-48 object-scale-down rounded-md" src={photo.url} alt="Auctioned object" />
