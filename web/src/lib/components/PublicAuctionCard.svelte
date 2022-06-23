f744eeb5ca89f76b45624def29bd97e311118a9a
Henry Romp <151henry151@gmail.com>
2022-06-23 12:46:45 -0400

    Center auction cards on home page and clean up styling.

--- web/src/lib/components/PublicAuctionCard.svelte
+++ web/src/lib/components/PublicAuctionCard.svelte
@@ -6,7 +6,7 @@
     export let auction : Auction;
 </script>
 
-<div class="glowbox">
+<div class="w-2/3 my-3 self-center glowbox">
 <div class="card md:card-side bg-base-300 max-w-full overflow-hidden shadow-xl my-3">
     <figure class="md:h-auto flex justify-center">
         {#each auction.media as photo, i}
@@ -16,47 +16,11 @@
         {/each}
     </figure>
     <div class="card-body">
-        <h2 class="card-title mb-2">
+        <h2 class="justify-center card-title mb-2">
             {auction.title}
         </h2>
-        {#if auction.started && !auction.ended}
-            <div class="float-root">
-                <div class="py-5 float-left">
-                    <div class="lg:flex">
-                        <div class="mt-4 lg:mt-0">
-                            <div class="ml-4 mt-2">
                                 <Countdown untilDate={auction.end_date} />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        {/if}
-        {#if !auction.ended}
-        <div class="mt-2">
-
-            <div class="pt-5 mb-5 w-full flex items-center justify-center rounded">
-                <ul class:lg:steps-horizontal={!auction.started} class="steps steps-vertical">
-                    <li class="step" class:lg:mb-5={!auction.started} class:lg:mr-5={!auction.started} class:step-primary={auction.started}>
-                            <div class="glowbutton glowbutton-view ml-2 mr-5 mb-5" on:click={() => { window.location.href = `${window.location.protocol}//${window.location.host}/auctions/${auction.key}`; }}></div>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        {/if}
-        <p class="whitespace-nowrap">
-                        {#if auction.start_date && auction.end_date}
-                From:
-                <DateFormatter date={auction.start_date} />
-                <br />
-                To:
-                <DateFormatter date={auction.end_date} />
-            {/if}
-            {#if auction.started}
-                <span class="lg:float-right">Bids: {auction.bids.length}</span>
-            {/if}
-        </p>
-
+                <span class="lg:float-right">{auction.bids.length} bids</span>
     </div>
 </div>
 </div>
