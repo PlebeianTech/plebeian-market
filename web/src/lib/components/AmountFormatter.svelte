637c316dd30779aad6d6fe2d880b1bcd124fb517
Ioan Bizău <i@ibz.me>
2022-07-04 09:41:36 +0300

    Simplify amount formatting.

--- web/src/lib/components/AmountFormatter.svelte
+++ web/src/lib/components/AmountFormatter.svelte
@@ -2,18 +2,4 @@
     export let amount;
 </script>
 
-{#if amount >= 1000000}
-    {#if amount % 1000000 === 0}
-        <span>{amount / 1000000}M</span>
-    {:else}
-        <span>{amount.toLocaleString('en')}</span>
-    {/if}
-{:else if amount >= 1000}
-    {#if amount % 1000 === 0}
-        <span>{amount / 1000}k</span>
-    {:else}
-        <span>{amount.toLocaleString('en')}</span>
-    {/if}
-{:else}
-    <span>{amount}</span>
-{/if}
+<span>{amount.toLocaleString('en')}</span>
