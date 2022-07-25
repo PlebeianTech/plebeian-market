c0a8ac439780d6dea5a0ca026cfa3a8be229340b
Joe Pasquantonio <joe.pasquantonio@gmail.com>
2022-07-25 12:13:23 -0400

    f

--- web/src/lib/components/Navbar.svelte
+++ web/src/lib/components/Navbar.svelte
@@ -77,7 +77,7 @@
         if (user.twitterProfileImageUrl) {
             return user.twitterProfileImageUrl
         }
-        return "/images/default-profile.png";
+        //return "/images/default-profile.png";
     }
 
     onMount(async () => {
@@ -120,7 +120,7 @@
         </a>
     </div>
     <div class="ml-auto pb-2 pt-1 pr-1" on:click={toggleMenu}>
-        {#if $token && $user}
+        {#if $token && $user && $user.twitterProfileImageUrl}
         <label for={null} tabindex="0" class:verified={$user.twitterUsernameVerified} class:not-verified={!$user.twitterUsernameVerified} class="btn btn-ghost btn-circle avatar ring-2 ring-primary ring-offset-0">
             <div class="w-10 rounded-full">
                 <img src={getProfileImage($user)} alt="Avatar" />
@@ -136,14 +136,14 @@
     </div>
     <div class="{menuDisplay} w-full">
         <ul class="flex flex flex-col" on:click={closeMenu}>
-            <a class="md:hidden px-3 py-2 rounded btn btn-ghost " href="{getBaseUrl()}about">
+            <a class="md:hidden px-3 py-2 rounded btn btn-ghost" href="{getBaseUrl()}about">
                 About
             </a>
-            <a class="md:hidden px-3 py-2 rounded btn btn-ghost cursor-pointer" href="{getBaseUrl()}faq">
+            <a class="md:hidden px-3 py-2 rounded btn btn-ghost" href="{getBaseUrl()}faq">
                 FAQ
             </a>
             {#if $user && !$user.twitterUsernameVerified}
-                <li class="pr-3 py-2 rounded btn btn-ghost cursor-pointer">
+                <li class="pr-3 py-2 rounded btn btn-ghost">
                     <label class="flex flex-row cursor-pointer pt-2" for="twitter-verification-modal" on:click|preventDefault={showTwitterVerification}>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" class="w-8 h-8 ml-2">
                             <path fill="rgb(255,0,0)" d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
