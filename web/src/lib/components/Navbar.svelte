ce271706699ec8104051450a294ab93d12a3dbe8
Joe Pasquantonio <joe.pasquantonio@gmail.com>
2022-07-26 13:38:47 -0400

    remove openMenu

--- web/src/lib/components/Navbar.svelte
+++ web/src/lib/components/Navbar.svelte
@@ -50,10 +50,6 @@
         menuDisplay = "hidden";
     }
 
-    function openMenu() {
-        menuDisplay = "block";
-    }
-
     function toggleTheme() {
         let html = <HTMLHtmlElement>document.querySelector('html');
         let toggle = <HTMLInputElement>document.getElementById('theme-toggle');
@@ -98,17 +94,17 @@
         {/if}
     </a>
     <div class="hidden md:block">
-        <a class="rounded btn btn-ghost normal-case text-xl ml-4" href={getBaseUrl()}>
+        <a class="rounded btn btn-ghost normal-case text-xl ml-4" href={getBaseUrl()} on:click={closeMenu}>
             Home
         </a>
-        <a class="rounded btn btn-ghost normal-case text-xl" href="{getBaseUrl()}about">
+        <a class="rounded btn btn-ghost normal-case text-xl" href="{getBaseUrl()}about" on:click={closeMenu}>
             About
         </a>
-        <a class="rounded btn btn-ghost normal-case text-xl" href="{getBaseUrl()}faq">
+        <a class="rounded btn btn-ghost normal-case text-xl" href="{getBaseUrl()}faq" on:click={closeMenu}>
             FAQ
         </a>
     </div>
-    <label class="rounded btn btn-ghost swap swap-rotate" on:click={toggleTheme}>
+    <label class="rounded btn btn-ghost swap swap-rotate" on:click={toggleTheme} on:click={closeMenu}>
         <input id="theme-toggle" type="checkbox" checked={prefersDark} />
         <!-- sun icon -->
         <svg class="swap-off fill-current w-10 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z"/></svg>
