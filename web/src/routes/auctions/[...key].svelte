8aac31d9c939cb4bca1073f235ebb340ededffa9
Luis Miguel García Mancebo <luismiguel@leanxcale.com>
2022-11-08 12:21:16 +0100

    Using svelte-meta-tags + preloading item data at the server to use

--- web/src/routes/auctions/[...key].svelte
+++ web/src/routes/auctions/[...key].svelte
@@ -1,17 +1,34 @@
 <script context="module">
-    export async function load({ params }) {
+    import { getBaseApiUrl } from "$lib/utils";
+
+    export async function load({ params, fetch }) {
         const { key } = params;
-        return { props: { itemKey: key } }
+        const auctionUrl = `${getBaseApiUrl()}/api/auctions/${key}`;
+        const response = await fetch(auctionUrl)
+        const auction = await response.json()
+        if (response.ok) {
+            return {
+                props: {
+                    itemKey: key,
+                    serverLoadedItem: auction.auction
+                }
+            }
+        }
+        return {
+            status: response.status,
+            error: new Error("Could not fetch auction on the server")
+        }
     }
 </script>
 
 <script lang="ts">
     import { goto } from "$app/navigation";
-    import ItemView from "$lib/components/ItemView.svelte"
+    import ItemView from "$lib/components/ItemView.svelte";
     import { fromJson } from "$lib/types/auction";
     import { onMount } from "svelte";
 
     export let itemKey;
+    export let serverLoadedItem;
 
     onMount(async () => {
         if (itemKey === "" || itemKey === null) {
@@ -21,5 +38,9 @@
 </script>
 
 {#if itemKey !== ""}
-    <ItemView {itemKey} loader={{endpoint: 'auctions', responseField: 'auction', fromJson }} />
+    <ItemView
+        {itemKey}
+        loader={{ endpoint: 'auctions', responseField: 'auction', fromJson }}
+        serverLoadedItem={fromJson(serverLoadedItem)}
+    />
 {/if}
--- web/src/routes/listings/[...key].svelte
+++ web/src/routes/listings/[...key].svelte
@@ -1,25 +1,46 @@
 <script context="module">
-    export async function load({ params }) {
+    import { getBaseApiUrl } from "$lib/utils";
+
+    export async function load({ params, fetch }) {
         const { key } = params;
-        return { props: { itemKey: key } }
+        const listingUrl = `${getBaseApiUrl()}/api/listings/${key}`;
+        const response = await fetch(listingUrl)
+        const listing = await response.json()
+        if (response.ok) {
+            return {
+                props: {
+                    itemKey: key,
+                    serverLoadedItem: listing.listing
+                }
+            }
+        }
+        return {
+            status: response.status,
+            error: new Error("Could not fetch listing on the server")
+        }
     }
 </script>
 
 <script lang="ts">
     import { goto } from "$app/navigation";
-    import ItemView from "$lib/components/ItemView.svelte"
+    import ItemView from "$lib/components/ItemView.svelte";
     import { fromJson } from "$lib/types/listing";
     import { onMount } from "svelte";
 
     export let itemKey;
+    export let serverLoadedItem;
 
     onMount(async () => {
         if (itemKey === "" || itemKey === null) {
-            goto('/');
+            goto("/");
         }
     });
 </script>
 
 {#if itemKey !== ""}
-    <ItemView {itemKey} loader={{endpoint: 'listings', responseField: 'listing', fromJson}} />
+    <ItemView
+        {itemKey}
+        loader={{ endpoint: "listings", responseField: "listing", fromJson }}
+        serverLoadedItem={fromJson(serverLoadedItem)}
+    />
 {/if}
