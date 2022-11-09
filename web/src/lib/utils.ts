2312da97a0d4e00585ed9836c714c1b3c2ecf165
Luis Miguel García Mancebo <luismiguel@leanxcale.com>
2022-11-09 11:03:23 +0100

    Adding env files with environment name

--- web/src/lib/utils.ts
+++ web/src/lib/utils.ts
@@ -1,33 +1,23 @@
 export let SATS_IN_BTC = 100000000;
 
 export function isLocal() {
-    // TODO: deal with local network addresses
-    // alternatively: pass an environment variable from the Dockerfile to node.js, in the same way we pass BASE_URL for the Python API => no more guessing based on the URL
-    return window.location.href.indexOf("localhost") !== -1 || window.location.href.indexOf("127.0.0.1") !== -1 || window.location.href.indexOf("0.0.0.0") !== -1;
+    return import.meta.env.MODE === 'development';
 }
 
 export function isStaging() {
-    return window.location.href.indexOf("staging") !== -1;
+    return import.meta.env.MODE === 'staging';
 }
 
 export function getBaseUrl() {
-    if (isLocal()) {
-        return "http://localhost:3000/";
-    } else if (isStaging()) {
-        return "https://staging.plebeian.market/";
-    } else {
-        return "https://plebeian.market/";
-    }
+    return import.meta.env.VITE_BASE_URL;
+}
+
+export function getApiBaseUrl() {
+    return import.meta.env.VITE_API_BASE_URL;
 }
 
 export function getEnvironmentInfo() {
-    if (isLocal()) {
-        return "local";
-    } else if (isStaging()) {
-        return "staging";
-    } else {
-        return "";
-    }
+    return import.meta.env.MODE;
 }
 
 export function sats2usd(sats: number, btc2usd: number | null): number | null {
