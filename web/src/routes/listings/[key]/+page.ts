c39a1af9986824af53e832b5f0b0f6fc97bb2aaa
Luis Miguel García Mancebo <luismiguel@leanxcale.com>
2022-11-11 12:32:30 +0100

    Also loading listings in the route

--- web/src/routes/listings/[key]/+page.ts
+++ web/src/routes/listings/[key]/+page.ts
@@ -1,4 +1,19 @@
-export function load({ params }) {
+import { getApiBaseUrl } from "$lib/utils";
+
+export async function load({ params }) {
     const { key } = params;
-    return { itemKey: key };
+
+    const listingUrl = `${getApiBaseUrl()}/api/listings/${key}`;
+    const response = await fetch(listingUrl)
+    const listing = await response.json()
+    if (response.ok) {
+        return {
+            itemKey: key,
+            serverLoadedItem: listing.listing
+        }
+    }
+    return {
+        status: response.status,
+        error: new Error("Could not fetch listing on the server")
+    }
 }
