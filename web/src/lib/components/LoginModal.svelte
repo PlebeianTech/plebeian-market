5b84e75f69b8d3315e6cf7c7bff400866c87471d
Luis Miguel García Mancebo <luismiguel@leanxcale.com>
2022-12-26 21:50:41 +0100

    Compensating <Login> margin-top with a negative value

--- web/src/lib/components/LoginModal.svelte
+++ web/src/lib/components/LoginModal.svelte
@@ -29,7 +29,7 @@
 <div class="modal">
     <div class="modal-box relative flex justify-center items-center w-10/12 max-w-2xl">
         <label for="modal-box" class="btn btn-sm btn-circle absolute right-2 top-2" on:click={() => hide()} on:keypress={() => hide()}>✕</label>
-        <div class="w-full">
+        <div class="w-full" style="margin-top: -40px">
             <svelte:component this={content} {onLogin} />
         </div>
     </div>
