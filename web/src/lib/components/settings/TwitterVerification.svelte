a27e31c1809631964b3c93a349088711a728c600
Luis Miguel García Mancebo <luismiguel@leanxcale.com>
2023-01-08 15:58:27 +0100

    Improve wording

--- web/src/lib/components/settings/TwitterVerification.svelte
+++ web/src/lib/components/settings/TwitterVerification.svelte
@@ -49,7 +49,7 @@
         <div class="alert alert-info shadow-lg">
             <div>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-current flex-shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                <span>You should have received three BIP-39 words via Twitter DM. This is your verification phrase.</span>
+                <span>Please enter the three BIP-39 words we sent to your Twitter DMs.</span>
             </div>
         </div>
         <div class="w-full flex items-center justify-center mt-4">
