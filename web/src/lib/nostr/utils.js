f1d833c2626ce06a7c098a75f1cdc5ae0432cfef
Luis Miguel García Mancebo <luismiguel@leanxcale.com>
2023-01-31 15:39:53 +0100

    nip05 backend support plus very basic frontend

--- web/src/lib/nostr/utils.js
+++ web/src/lib/nostr/utils.js
@@ -1,33 +1,26 @@
-import {getEventHash, nip05} from "nostr-tools";
+import {getEventHash, nip05, Kind} from "nostr-tools";
 
 export const relayUrlList = [
-    "nostr.mwmdev.com",
-    "wss://nostr-relay.alekberg.net",
-    "wss://nostr-pub.wellorder.net",
-    "wss://relay.current.fyi",
-    "wss://nostr.bitcoiner.social",
+    // Amethyst relays
     "wss://relay.damus.io",
-    "wss://nostr.kollider.xyz",
-    "relay.nostrich.de",
-    "wss://relay.nostr.ro",
-    "wss://relay.nostr.info",
-    "wss://nostr.zebedee.cloud",
-    "wss://nostr-pub.semisol.dev",
-    "wss://nostr.walletofsatoshi.com"
+    "wss://relay.nostr.bg",
+    "wss://nostr.mom",
+    "wss://nos.lol",
+    "wss://nostr.bitcoiner.social",
+    "wss://nostr-pub.wellorder.net",
+    "wss://nostr.wine",
+    "wss://eden.nostr.land",
+    "wss://relay.orangepill.dev",
+    "wss://no.str.cr",  // *
+    "wss://puravida.nostr.land",
+    "wss://relay.nostr.com.au",
+    "wss://nostr.inosta.cc",
 ];
 
 export const pmMasterPublicKey = '03b5036dc3db82604307c1964d2b926417a91c3b11ef75ba6ca55019e9b7a62a';
 export const pmChannelNostrRoomId = '4211fc228be5af10923f56e60b1b11b8e63bf0ac7dbd3e1e3d767392fdaed4a4'; // Directo 2140
 // '25e5c82273a271cb1a840d0060391a0bf4965cafeb029d5ab55350b418953fbb';   // 'Nostr' channel
 export const localStorageNostrPreferPMId = 'nostr-prefer-pm-identity';
-export const nostrEventKinds = {
-    'metadata': 0,
-    'note': 1,
-    'replies': 6,
-    'reactions': 7,
-    'createChannel': 40,
-    'channelNote': 42
-}
 
 export function hasExtension() {
     return !!window.nostr;
@@ -56,7 +49,7 @@ export function getChannelIdForStallOwner(user) {
     let created_at = 1672837282;
 
     let event = {
-        kind: nostrEventKinds.createChannel,
+        kind: Kind.ChannelCreation,
         pubkey: pmMasterPublicKey,
         created_at: created_at,
         content: '{"name": "' + stallName + '", "about": "Plebeian Market Stall Square."}',
