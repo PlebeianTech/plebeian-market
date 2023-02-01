60d5ed0445ca4a53e8808d9f29907c0a86952da0
Luis Miguel García Mancebo <luismiguel@leanxcale.com>
2023-02-01 10:13:45 +0100

    Unify all 3 "XPUB infos" into a component so we only touch one place

---
+++ web/src/lib/components/XpubInfo.svelte
@@ -0,0 +1,11 @@
+<div class="alert alert-info shadow-lg">
+    <div>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-current flex-shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+        <span>
+                                <p>We use your XPUB to generate addresses for your payments.
+                                    We strongly suggest you use a separate wallet for this campaign only!</p>
+                                <p><b>RECOMMENDED WALLETS</b>: Blue, Wasabi, Electrum, Sparrow, Samourai.</p>
+                                <p><b>DO NOT USE</b>: Muun, Breez or Phoenix (<a class="link" href="/faq" target="_blank" rel="noreferrer">why?</a>)</p>
+                            </span>
+    </div>
+</div>
