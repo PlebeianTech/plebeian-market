3ce654cb36f7d8476f6f932cece93f6feda035f5
jaykhan-dev <jaykhan.sound@gmail.com>
2023-02-03 15:51:27 -0500

    nostr and items list are flex, removed icon graphics for buttons, nostr chat is sticky

--- web/src/lib/components/StallView.svelte
+++ web/src/lib/components/StallView.svelte
@@ -298,20 +298,20 @@
                     {showItemsOwner} {showItemsCampaign}
                     card={ItemCard}
                     style={ListViewStyle.List}>
-                    <div slot="new-entity" class="lg:flex lg:justify-start justify-center" let:setCurrent={setCurrent}>
+                    <div slot="new-entity" class="lg:flex items-center lg:justify-start justify-center" let:setCurrent={setCurrent}>
                         {#if isCampaignStall}
-                            <div id="auction-hour-1" class="grid lg:place-items-start w-full p-4" on:click|preventDefault={() => loginAndNewItem(setCurrent, () => new TimeAuction())}>
-                              <div class="w-full my-8 grid place-items-center">
+                            <div id="auction-hour-1" class="" on:click|preventDefault={() => loginAndNewItem(setCurrent, () => new TimeAuction())}>
+                              <div class="grid place-items-center">
                                 <!-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width={1.5} stroke="currentColor" class="w-24 h-24">
                                   <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
                                 </svg> -->
-                                <button class="btn btn-primary font-bold text-center">1 Hour of your time</button>
+                                <button class="btn btn-primary font-bold text-center w-52">1 Hour of your time</button>
                               </div>
                             </div>
                         {/if}
 
                         <div id="anchorIdAuctionItem" class="grid lg:place-items-start place-items-center w-full p-4" on:click|preventDefault={() => loginAndNewItem(setCurrent, () => new Auction())}>
-                          <div class="w-full my-4">
+                          <div class="w-full flex lg:justify-start justify-center">
                             <!-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-24 h-24">
                               <path stroke-linecap="round" stroke-linejoin="round" d="M12 3.75v16.5M2.25 12h19.5M6.375 17.25a4.875 4.875 0 004.875-4.875V12m6.375 5.25a4.875 4.875 0 01-4.875-4.875V12m-9 8.25h16.5a1.5 1.5 0 001.5-1.5V5.25a1.5 1.5 0 00-1.5-1.5H3.75a1.5 1.5 0 00-1.5 1.5v13.5a1.5 1.5 0 001.5 1.5zm12.621-9.44c-1.409 1.41-4.242 1.061-4.242 1.061s-.349-2.833 1.06-4.242a2.25 2.25 0 013.182 3.182zM10.773 7.63c1.409 1.409 1.06 4.242 1.06 4.242S9 12.22 7.592 10.811a2.25 2.25 0 113.182-3.182z" />
                             </svg> -->
@@ -356,7 +356,7 @@
                     card={ItemCard}
                     style={ListViewStyle.List}>
                     <div slot="new-entity" class="flex justify-start" let:setCurrent={setCurrent}>
-                        <div class="my-4 grid w-full p-4" on:click|preventDefault={() => loginAndNewItem(setCurrent, () => new Listing())}>
+                        <div class="my-4 w-full p-4 flex lg:justify-start justify-center" on:click|preventDefault={() => loginAndNewItem(setCurrent, () => new Listing())}>
                           <!-- <div class="w-20 my-8">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
                               <path stroke-linecap="round" stroke-linejoin="round" d="M12 3.75v16.5M2.25 12h19.5M6.375 17.25a4.875 4.875 0 004.875-4.875V12m6.375 5.25a4.875 4.875 0 01-4.875-4.875V12m-9 8.25h16.5a1.5 1.5 0 001.5-1.5V5.25a1.5 1.5 0 00-1.5-1.5H3.75a1.5 1.5 0 00-1.5 1.5v13.5a1.5 1.5 0 001.5 1.5zm12.621-9.44c-1.409 1.41-4.242 1.061-4.242 1.061s-.349-2.833 1.06-4.242a2.25 2.25 0 013.182 3.182zM10.773 7.63c1.409 1.409 1.06 4.242 1.06 4.242S9 12.22 7.592 10.811a2.25 2.25 0 113.182-3.182z" />
