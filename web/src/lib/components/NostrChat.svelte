452ec39a56bf74070a69a4f0027383fd8b5f56eb
Luis Miguel García Mancebo <luismiguel@leanxcale.com>
2023-02-08 21:10:17 +0100

    Deleting this for the nth time

--- web/src/lib/components/NostrChat.svelte
+++ web/src/lib/components/NostrChat.svelte
@@ -52,11 +52,6 @@
     // true: requested
     let notesMap = new Map();
 
-    // null: to be requested
-    // false: requested but error (so don't ask again)
-    // other: the nip05 public key
-    let nip05 = new Map();
-
     const pool: Pool = new Pool();
 
     function orderAndVitamineMessages() {
@@ -466,7 +461,7 @@
                  hover:scale-110 duration-300 transition-all cursor-pointer text-white">
                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-                </svg>                
+                </svg>
             </div>
         </div>
     {/if}
