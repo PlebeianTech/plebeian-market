7a08b8eeeb19ccfba1e0667f754078a7cd9dcfaa
Luis Miguel García Mancebo <luismiguel@leanxcale.com>
2023-02-17 13:52:22 +0100

    Enabling Nostr as it's ready for Production

--- web/src/routes/marketsquare/+page.svelte
+++ web/src/routes/marketsquare/+page.svelte
@@ -4,18 +4,15 @@
 
 <script lang="ts">
     import NostrChat from "$lib/components/nostr/Chat.svelte";
-    import { isProduction } from "$lib/utils";
     import { pmChannelNostrRoomId } from '$lib/nostr/pool'
 </script>
 
 <div class="lg:w-2/3 mx-auto lg:p-0 p-2">
   <h1 class="lg:text-6xl text-3xl font-bold lg:mt-8 mt-4">Plebeian Market Square</h1>
     <div class="py-4">
-        {#if !isProduction() }
-            <NostrChat
-                messageLimit={75}
-                nostrRoomId={pmChannelNostrRoomId}
-            />
-        {/if}
+        <NostrChat
+            messageLimit={75}
+            nostrRoomId={pmChannelNostrRoomId}
+        />
     </div>
 </div>
