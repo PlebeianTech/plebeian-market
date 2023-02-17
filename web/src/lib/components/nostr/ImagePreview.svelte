cfb97ab1911677af4616a24363f1c7095694e305
Luis Miguel García Mancebo <luismiguel@leanxcale.com>
2023-02-17 18:31:40 +0100

    Image previews on mobile must occupy just the whole screen, not more

--- web/src/lib/components/nostr/ImagePreview.svelte
+++ web/src/lib/components/nostr/ImagePreview.svelte
@@ -2,6 +2,6 @@
     export let imagePreviewUrl;
 </script>
 
-<div class="card w-96 bg-base-100 shadow-xl">
+<div class="card bg-base-100 shadow-xl w-full lg:w-96">
     <figure><img class="rounded-xl" src="{imagePreviewUrl}" alt="Image preview" /></figure>
 </div>
