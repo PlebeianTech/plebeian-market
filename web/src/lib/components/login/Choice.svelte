99cb229b36d36bdec4f75a481396c7ab91031554
Ioan Bizău <i@ibz.me>
2023-02-21 14:27:33 +0200

    Do not show Nostr log in on production.

--- web/src/lib/components/login/Choice.svelte
+++ web/src/lib/components/login/Choice.svelte
@@ -2,6 +2,7 @@
     import { createEventDispatcher, onMount } from "svelte";
     import LnurlLogin from "$lib/components/login/Lnurl.svelte";
     import NostrLogin from "$lib/components/login/Nostr.svelte";
+    import { isProduction } from "$lib/utils";
 
     const dispatch = createEventDispatcher();
 
@@ -20,7 +21,9 @@
 <div class="w-full flex items-center justify-center mt-4">
     <div class="tabs tabs-boxed">
         <a class="tab tab-lg" class:tab-active={type === LoginType.Lnurl} href={null} on:click={() => type = LoginType.Lnurl}>LNURL</a>
-        <a class="tab tab-lg" class:tab-active={type === LoginType.Nostr} href={null} on:click={() => type = LoginType.Nostr}>Nostr</a>
+        {#if !isProduction()}
+            <a class="tab tab-lg" class:tab-active={type === LoginType.Nostr} href={null} on:click={() => type = LoginType.Nostr}>Nostr</a>
+        {/if}
     </div>
 </div>
 
