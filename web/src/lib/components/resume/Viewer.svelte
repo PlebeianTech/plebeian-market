b1af19b0e49c6c7099f08dd98e6ead7c7c7c6088
Luis Miguel García Mancebo <luismiguel@leanxcale.com>
2023-03-13 15:58:35 +0100

    More UI improvements for the Skills Market

--- web/src/lib/components/resume/Viewer.svelte
+++ web/src/lib/components/resume/Viewer.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import { onMount } from 'svelte';
     import type { UserResume } from "$lib/types/user";
+    import profilePicturePlaceHolder from "$lib/images/profile_picture_placeholder.svg";
 
     export let resume: UserResume;
     export let name: string | null;
@@ -15,24 +16,29 @@
     );
 </script>
 
-<div class="flex justify-center items-center mt-4 h-15 gap-8">
-    <button class="btn btn-primary btn-lg" on:click|preventDefault={onViewFinished}>Back</button>
+<div class="flex items-center mt-4 h-15 gap-8">
+    <button class="btn btn-primary btn-outline" on:click|preventDefault={onViewFinished}>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mr-2">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M21 16.811c0 .864-.933 1.405-1.683.977l-7.108-4.062a1.125 1.125 0 010-1.953l7.108-4.062A1.125 1.125 0 0121 8.688v8.123zM11.25 16.811c0 .864-.933 1.405-1.683.977l-7.108-4.062a1.125 1.125 0 010-1.953L9.567 7.71a1.125 1.125 0 011.683.977v8.123z" />
+        </svg>
+        Back to the Skills market
+    </button>
 </div>
 
-<div class="card bg-base-300 shadow-xl mt-4">
-    <figure>
-        <img src={picture} alt="" />
+<div class="card bg-base-300 shadow-xl mt-4 pt-6 place-items-center items-center">
+    <figure class="avatar mask mask-squircle h-80 w-80">
+        <img src={picture ?? profilePicturePlaceHolder} alt="" />
     </figure>
     <div class="card-body">
-        <h2 class="card-title text-3xl">{name}</h2>
+        <h2 class="text-3xl text-center">{name}</h2>
 
-        <div class="text-center mt-8 text-3xl">
+        <div class="text-center mt-4 text-3xl">
             {resume.jobTitle}
         </div>
 
         <div class="flex flex-row gap-2 items-center justify-center">
             {#each resume.skills as skill}
-                <div class="badge badge-primary badge-lg mt-4">{skill.skill}</div>
+                <div class="badge badge-outline badge-primary badge-lg mt-4">{skill.skill}</div>
             {/each}
         </div>
 
@@ -126,4 +132,13 @@
             </div>
         {/if}
     </div>
+</div>
+
+<div class="flex items-center mt-4 h-15 gap-8">
+    <button class="btn btn-primary btn-outline" on:click|preventDefault={onViewFinished}>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mr-2">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M21 16.811c0 .864-.933 1.405-1.683.977l-7.108-4.062a1.125 1.125 0 010-1.953l7.108-4.062A1.125 1.125 0 0121 8.688v8.123zM11.25 16.811c0 .864-.933 1.405-1.683.977l-7.108-4.062a1.125 1.125 0 010-1.953L9.567 7.71a1.125 1.125 0 011.683.977v8.123z" />
+        </svg>
+        Back to the Skills market
+    </button>
 </div>
--- web/src/routes/skills/+page.svelte
+++ web/src/routes/skills/+page.svelte
@@ -15,7 +15,7 @@
 </script>
 
 <div class="px-2 pb-4 lg:pb-14 mx-auto w-screen lg:w-2/3 p-2 py-7">
-    <h1 class="text-3xl lg:text-6xl fontbold mt-0 lg:mt-8 mb-2 text-center">Skills Market</h1>
+    <h1 class="text-3xl lg:text-6xl fontbold mt-0 lg:mt-2 mb-4 text-center">Skills Market</h1>
 
     {#if editing}
         <ResumeEditor onEditFinished={() => editing = false} />
@@ -23,7 +23,7 @@
         <ResumeViewer resume={viewingResume} name={viewingResumeName} picture={viewingResumePicture} onViewFinished={() => viewingResume = null} />
     {:else}
         <div class="flex justify-center items-center">
-            <button class="btn btn-lg btn-primary" on:click={() => editing = true}>Edit Résumé</button>
+            <button class="btn btn-primary" on:click={() => editing = true}>Edit My Résumé</button>
         </div>
 
         <div class="divider"></div>
