e7a9f5529e57d1bb803ba34702608f889ec05d4c
Luis Miguel García Mancebo <luismiguel@leanxcale.com>
2023-04-11 15:30:09 +0200

    Moving SVGs to svelte components

---
+++ web/src/lib/components/icons/Substack.svelte
@@ -0,0 +1,3 @@
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);">
+    <path fill="currentColor" d="M22.539 8.242H1.46V5.406h21.08v2.836zM1.46 10.812V24L12 18.11L22.54 24V10.812H1.46zM22.54 0H1.46v2.836h21.08V0z"/>
+</svg>
