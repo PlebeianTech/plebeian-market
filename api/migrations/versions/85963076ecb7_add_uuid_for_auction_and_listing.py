cc7bd4b1e5bd2b19daa9e2e6ee9c8a79aa19217a
Ioan Bizău <i@ibz.me>
2023-06-16 10:28:15 +0300

    text()

--- api/migrations/versions/85963076ecb7_add_uuid_for_auction_and_listing.py
+++ api/migrations/versions/85963076ecb7_add_uuid_for_auction_and_listing.py
@@ -6,7 +6,7 @@ Create Date: 2023-06-16 07:08:41.345520
 
 """
 from alembic import op
-import sqlalchemy as sa
+import sqlalchemy as sa, text
 
 # revision identifiers, used by Alembic.
 revision = '85963076ecb7'
@@ -16,11 +16,11 @@ depends_on = None
 
 def upgrade():
     with op.batch_alter_table('auctions', schema=None) as batch_op:
-        batch_op.add_column(sa.Column('uuid', sa.UUID(), nullable=False, server_default="gen_random_uuid()"))
+        batch_op.add_column(sa.Column('uuid', sa.UUID(), nullable=False, server_default=text("gen_random_uuid()")))
         batch_op.create_index(batch_op.f('ix_auctions_uuid'), ['uuid'], unique=True)
 
     with op.batch_alter_table('listings', schema=None) as batch_op:
-        batch_op.add_column(sa.Column('uuid', sa.UUID(), nullable=False, server_default="gen_random_uuid()"))
+        batch_op.add_column(sa.Column('uuid', sa.UUID(), nullable=False, server_default=text("gen_random_uuid()")))
         batch_op.create_index(batch_op.f('ix_listings_uuid'), ['uuid'], unique=True)
 
 def downgrade():
