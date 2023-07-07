f88429d2eefe13f7a19102b114f6ba0b9650ced5
Luis Miguel García Mancebo <luismiguel@leanxcale.com>
2023-07-07 14:21:14 +0200

    Small UI improvements for "view resume" screen

--- web/frontoffice/src/lib/components/resume/View.svelte
+++ web/frontoffice/src/lib/components/resume/View.svelte
@@ -3,11 +3,11 @@
     import { page } from "$app/stores";
     import type { UserResume } from "$lib/types/user";
     import Editor from "$lib/components/resume/Editor.svelte";
+    import InfoBox from "$lib/components/notifications/InfoBox.svelte";
     import {newNostrConversation} from "$lib/nostr/utils";
     import { subscribeResume, subscribeMetadata } from "$lib/services/nostr";
     import profilePicturePlaceHolder from "$lib/images/profile_picture_placeholder.svg";
     import {NostrPublicKey} from "$lib/stores";
-    import InfoBox from "$lib/components/notifications/InfoBox.svelte";
 
     export let pubkey: string;
 
@@ -59,7 +59,7 @@
 <div class="mx-auto w-full mt-16">
     {#if !resume}
         {#if pubkey === $NostrPublicKey}
-            <InfoBox classText="text-xl">You're not <b>selling your time/services</b> to the community.<br />
+            <InfoBox>You're not <b>selling your time/services</b> to the community.<br />
                 You can <a class="underline" href="/p/{$NostrPublicKey}#edit"><b>do it here</b></a> by creating your resume.</InfoBox>
         {/if}
     {:else}
@@ -80,10 +80,11 @@
                     <img src={picture ?? profilePicturePlaceHolder} alt="" />
                 </figure>
                 <div class="card-body w-full px-3">
-                    <h2 class="text-3xl text-center">{name}</h2>
+
+                    <h2 class="text-3xl text-center">{name ?? ''}</h2>
 
                     <div class="text-center mt-2 text-3xl">
-                        {resume.jobTitle}
+                        {resume.jobTitle ?? ''}
                     </div>
 
                     <div class="flex flex-wrap items-center justify-center w-full mt-2 text-center gap-2">
