cb61a973c8383600a2a24f65b75cb0e9c2fb4fa7
Luis Miguel García Mancebo <luismiguel@leanxcale.com>
2023-07-28 16:02:35 +0200

    Big UX improvement to the login modal

--- web/shared/src/lib/components/login/Nostr.svelte
+++ web/shared/src/lib/components/login/Nostr.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import { createEventDispatcher, onMount } from 'svelte';
     import {NostrPrivateKey, NostrPublicKey} from "$sharedLib/stores";
-    import { hasExtension, encodeNpub, tryLoginToBackend } from '$sharedLib/nostr/utils';
+    import { hasExtension, tryLoginToBackend } from '$sharedLib/nostr/utils';
     import {generatePrivateKey, getPublicKey} from "nostr-tools";
     import { browser } from "$app/environment";
     import AlertInfo from "$sharedLib/components/icons/AlertInfo.svelte";
@@ -10,7 +10,6 @@
 
     const dispatch = createEventDispatcher();
 
-    let npub: string | null = null;
     let activeTab: number = 0;
     let newPrivateKey: string | null = null;
     let showPrivateKeyInfo: boolean = false;
@@ -18,14 +17,12 @@
     $: if ($NostrPublicKey) {
         // If login was called because $NostrPublicKey was not available
         // but immediately after it became available, dispatch a login
-        // event so the dialog is closed
+        // event so the dialog is closed asap
         dispatch('login', {});
     }
 
     async function getKeyFromExtension() {
         let publicKey = await (window as any).nostr.getPublicKey();
-
-        npub = encodeNpub(publicKey);
 
         setLoginMethod('extension');
 
@@ -85,20 +82,17 @@
         dispatch('close', {});
     }
 
-    function activateFirstTabIfExtensionPresent() {
-        if (browser && hasExtension())
-            activeTab=0;
-    }
-
     onMount(async () => {
         if (browser && !hasExtension()) {
             activeTab = 1;
         }
 
+        /*
         if ($NostrPrivateKey && !$NostrPublicKey) {
             console.log('Nostr Public Key not available (because logged-out previously?) but Private Key present. Logging in...');
-            // await savePrivateNostrKey($NostrPrivateKey);
+            await savePrivateNostrKey($NostrPrivateKey);
         }
+        */
     });
 </script>
 
@@ -127,75 +121,58 @@
 
     <div>
         <div class="tabs">
-            {#if browser && hasExtension()}
-                <a
-                   data-tip="You need a Nostr browser extension"
-                   class="indicator tab tab-lifted {browser && hasExtension() ? '' : 'tab-disabled tooltip tooltip-bottom tooltip-error'} tab-sm md:tab-lg flex-1 p-4 pb-8 lg:pb-6 lg:py-2 {activeTab===0 ? 'bg-base-300 text-base-content' : ''}" on:click={activateFirstTabIfExtensionPresent}>
-                    <span class="indicator-item indicator-center badge badge-md badge-error">recommended</span>
-                    Use Nostr extension
-                </a>
-            {/if}
-            <a class="indicator tab tab-lifted tab-sm md:tab-lg flex-1 p-4 pb-8 lg:pb-6 lg:py-2 {activeTab===1 ? 'bg-base-300 text-base-content' : ''}" on:click={() => activeTab=1}>
-                <span class="indicator-item indicator-center badge badge-md badge-warning">anonymous</span>
-                Generate New Key
-            </a>
-            <a class="tab tab-lifted tab-sm md:tab-lg flex-1 p-4 pb-8 lg:pb-6 lg:py-2 {activeTab===2 ? 'bg-base-300 text-base-content' : ''}" on:click={() => activeTab=2}>Use your private key</a>
+            <a class="tab tab-lifted tab-sm md:tab-lg flex-1 p-4 pb-8 lg:pb-6 lg:py-2 {activeTab===0 ? 'bg-base-300 text-base-content' : ''}" on:click={() => activeTab=0}>Existing Nostr user</a>
+            <a class="tab tab-lifted tab-sm md:tab-lg flex-1 p-4 pb-8 lg:pb-6 lg:py-2 {activeTab===1 ? 'bg-base-300 text-base-content' : ''}" on:click={() => activeTab=1}>New user / Anon</a>
         </div>
 
         <div class="grid w-full flex-grow gap-3 p-4 py-6 lg:p-14 lg:py-8 bg-base-300 rounded-xl rounded-tl-none rounded-tr-none md:shadow-xl items-center justify-center">
             {#if activeTab===0}
-                <div class="w-full flex">
-                    <div class="form-control w-full max-w-full">
-                        <p>A Nostr extension is the <b>most secure</b> and <b>recommended</b> way to use Plebeian Market.</p>
-                        <p class="mt-4">By using the identity that you created in your Nostr extension, you will be <b>building your reputation</b> each time you buy or sell any product and act as a good pleb by paying on time or shipping in good order.</p>
-                        {#if npub}
-                            <label class="label mt-8" for="npub">
-                                <span class="label-text">Your NPUB</span>
-                            </label>
+                {#if hasExtension}
+                    <div class="flex mt-6 items-center justify-center text-warning">
+                        Recommended
+                    </div>
+                    <div class="flex w-full items-center justify-center">
+                        <button class="btn btn-success" on:click={getKeyFromExtension}>Use Nostr browser extension</button>
+                    </div>
 
-                            <input bind:value={npub} type="text" id="npub" name="npub" class="input md:input-lg input-bordered" />
-                        {/if}
+                    <div class="divider py-6 md:py-8">OR</div>
+                {/if}
+
+                <div class="w-full flex items-center justify-center">
+                    <div class="form-control w-full max-w-full">
+                        <input class="input md:input-md input-bordered" bind:value={newPrivateKey} type="text" placeholder="Paste your Nostr private key" />
+                        <p class="mt-4 mb-2 text-xs">It will be stored in the web browser of this device, so Plebeian Market will have no way to access it.</p>
                     </div>
                 </div>
-                <div class="w-full flex items-center justify-center mt-8 gap-5">
-                    <button class="btn btn-success" on:click={getKeyFromExtension}>Use Nostr browser extension</button>
+
+                <div class="w-full flex items-center justify-center">
+                    <button class="btn btn-primary" on:click={saveProvidedNostrKey}>Save the private key</button>
                 </div>
 
             {:else if activeTab===1}
-                <div class="w-full flex">
+                <div class="w-full flex pt-16">
                     <div class="form-control w-full max-w-full">
-                        <p>With this option you can let us <b>generate a new Nostr private key</b> for you. In this way you are also essentially <b>buying products anonymously</b> because the freshly generated key is not associated with any other identities you may be already using on Nostr.</p>
-                        <p class="mt-4">This is the recommended option if you don't have a Nostr extension or a Nostr identity already created.</p>
+                        <p class="mb-4">
+                            This is the way to start using Plebeian Market if you don't have a Nostr extension or you want to have an anonymous identity.
+                            <button class="btn btn-outline btn-xs btn-info" on:click={() => {window.modal_generate_key.showModal()}}>More info</button>
+                        </p>
+
 
                         {#if $NostrPrivateKey}
                             <div class="form-control w-full max-w-full mt-8">
-                                <p class="mb-4">This is the key we generated for you. You can use it or generate a new one.</p>
+                                <p class="mb-4">This is the key we previously generated for you. You can use it or generate a new one.</p>
                                 <p class="mb-4"><b>If you generate a new one, you'll better make sure you save this one to a secure place before, or you'll lose your ability to communicate with sellers and keep the Orders history.</b></p>
                                 <input bind:value={$NostrPrivateKey} type="text" class="input md:input-lg input-bordered" />
                             </div>
-                        {:else}
-                            <p class="mt-4">A new Nostr private key will be <b>generated and stored in the web browser of this device</b>, so no other person will have access to it.</p>
                         {/if}
                     </div>
                 </div>
 
-                <div class="w-full flex items-center justify-center mt-3">
+                <div class="w-full flex items-center justify-center mt-8 mb-12">
                     {#if $NostrPrivateKey}
                         <button class="btn btn-success btn-xs md:btn-md mr-8" on:click={useSameKeyToLogin}>Use this key</button>
                     {/if}
-                    <button class="btn btn-warning btn-xs md:btn-md" on:click={generateNewNostrKey}>Generate a new Nostr key</button>
-                </div>
-
-            {:else if activeTab===2}
-                <div class="w-full flex items-center justify-center">
-                    <div class="form-control w-full max-w-full">
-                        <p class="mb-4 md:mb-6">Paste your Nostr private key. It will be stored in the web browser of this device, so Plebeian Market will have no way to access it. The only thing we will access to is the public key associated to this private key!</p>
-                        <input bind:value={newPrivateKey} type="text" class="input md:input-lg input-bordered" />
-                    </div>
-                </div>
-
-                <div class="w-full flex items-center justify-center mt-3">
-                    <button class="btn btn-primary" on:click={saveProvidedNostrKey}>Save the private key</button>
+                    <button class="btn btn-primary btn-xs md:btn-md" on:click={generateNewNostrKey}>Generate a new Nostr key</button>
                 </div>
             {/if}
         </div>
@@ -204,6 +181,20 @@
     <PrivateKeyInfo showActionButtons={false} />
 
     <div class="flex justify-center items-center">
-        <button id="close-window" class="btn btn-error btn-sm md:btn-md" on:click|preventDefault={closeLoginModal}>I understand, close this</button>
+        <button id="close-window" class="btn btn-success btn-sm md:btn-md" on:click|preventDefault={closeLoginModal}>I understand, close this</button>
     </div>
 {/if}
+
+<dialog id="modal_generate_key" class="modal">
+    <form method="dialog" class="modal-box">
+        <p class="mt-4">With this option you can let us <b>generate a new Nostr private key</b> for you. In this way you are also essentially <b>buying products anonymously</b>, because the freshly generated key is not associated with any other identities you may be already using on Nostr.</p>
+        <p class="mt-4">This is the recommended option if you don't have a Nostr extension or a Nostr identity already created.</p>
+        <p class="mt-4">A new Nostr private key will be <b>generated and stored in the web browser of this device</b>, so no other person will have access to it.</p>
+        <div class="modal-action">
+            <button on:click={window.modal_generate_key.close()} class="btn">Close</button>
+        </div>
+    </form>
+    <form method="dialog" class="modal-backdrop">
+        <button>close</button>
+    </form>
+</dialog>
