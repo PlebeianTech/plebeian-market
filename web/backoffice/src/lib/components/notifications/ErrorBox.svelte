b2d6ae8719822bc8a8bb38ac9cbc41f1c3fa04ee
Luis Miguel García Mancebo <luismiguel@leanxcale.com>
2023-07-31 17:08:23 +0200

    Update alerts after the migration to daisyUI v3

--- web/backoffice/src/lib/components/notifications/ErrorBox.svelte
+++ web/backoffice/src/lib/components/notifications/ErrorBox.svelte
@@ -4,14 +4,12 @@
 </script>
 
 <div class="alert alert-error shadow-lg">
+    <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
     <div>
-        <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-        <div>
-            <slot />
-            {#if hasDetail && detailVisible}
-                <slot name="detail" />
-            {/if}
-        </div>
+        <slot />
+        {#if hasDetail && detailVisible}
+            <slot name="detail" />
+        {/if}
     </div>
     <div>
         {#if hasDetail && !detailVisible}
