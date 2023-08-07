06ac0b824a274a11510dc3ec92882cb88d2b30f1
ktecho <ktecho@ktecho.com>
2023-08-07 19:31:05 +0200

    Support changing classes in the Title-h1 component

--- web/shared/src/lib/components/layout/Title-h1.svelte
+++ web/shared/src/lib/components/layout/Title-h1.svelte
@@ -1,3 +1,6 @@
-<h1 class="p-4 mt-4 md:mt-12 mb-4 md:mb-12 text-center text-3xl lg:text-3xl">
+<script lang="ts">
+    export let titleClass = "p-4 mt-4 md:mt-12 mb-4 md:mb-12 text-center text-3xl lg:text-3xl";
+</script>
+<h1 class="{titleClass}">
     <slot></slot>
 </h1>
