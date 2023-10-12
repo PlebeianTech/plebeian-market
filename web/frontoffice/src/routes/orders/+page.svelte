a6451590e2ea70c0d8c2eeabb46935113ae252b5
ktecho <ktecho@ktecho.com>
2023-10-12 18:27:01 +0200

    New PaymentWidget component to be used in Orders and while buying badges

--- web/frontoffice/src/routes/orders/+page.svelte
+++ web/frontoffice/src/routes/orders/+page.svelte
@@ -2,28 +2,20 @@
     import {products} from "$lib/stores";
     import {NostrPublicKey, stalls, privateMessages, Info} from "$sharedLib/stores";
     import {formatTimestamp, newNostrConversation, pmStallId} from "$sharedLib/nostr/utils";
-    import QRLocal from "$lib/components/QRLocal.svelte";
     import {refreshProducts, refreshStalls} from "$lib/shopping";
     import Titleh1 from "$sharedLib/components/layout/Title-h1.svelte";
     import Bitcoin from "$sharedLib/components/icons/Bitcoin.svelte";
     import Clock from "$sharedLib/components/icons/Clock.svelte";
     import {waitAndShowLoginIfNotLoggedAlready} from "$sharedLib/utils";
     import {onDestroy, onMount} from "svelte";
-    import {bech32} from "bech32";
-    import {Buffer as BufferPolyfill} from "buffer";
     import { afterNavigate } from "$app/navigation";
     import EmailIcon from "$sharedLib/components/icons/Email.svelte";
+    import PaymentWidget from "$lib/components/stores/PaymentWidget.svelte";
 
     let paymentModalVisible = false;
     let paymentConfirmationModalVisible = false;
 
-    let paymentInfo = {
-        link: null,
-        protocol: null,
-        amount: null
-    };
     let orderToBePaid = null;
-    let paymentOptionSelected = null;
 
     let sortedOrders = [];
     let ordersToBePaidNow = [];
@@ -34,6 +26,8 @@
     let showAutomaticPayments = false;
 
     $: paidPaymentsStorage = [];
+
+    let showPaymentDetails;
 
     $: {
         sortedOrders = Object.entries($privateMessages.automatic)
@@ -75,11 +69,11 @@
                 if (payment_option.type === 'ln') {
                     if (Date.now() > ((orderToPay.created_at * 1000) + (payment_option.expiry * 1000))) {
                         orderToBePaid = orderToPay;
-                        showPaymentDetails();
+                        paymentModalVisible = true;
                     }
                 } else {
                     orderToBePaid = orderToPay;
-                    showPaymentDetails();
+                    paymentModalVisible = true;
                 }
             }
         }
@@ -88,94 +82,12 @@
         paymentModalVisible = false;
     }
 
-    function bech32Encode(url) {
-        let words = bech32.toWords(BufferPolyfill.from(url, 'utf8'))
-        return bech32.encode('lnurl', words, 1500)
-    }
-
-    function getCurrentPaymentOptionInfo() {
-        for (const option of orderToBePaid.payment_options) {
-            if (option.type === paymentOptionSelected) {
-                return option;
-            }
-        }
-    }
-
     function getAmountToPay(order) {
         for (const option of order.payment_options) {
             if (option.amount_sats) {
                 return option.amount_sats;
             }
         }
-    }
-
-    function showPaymentDetails() {
-        if (!paymentOptionSelected) {
-            // No payment option selected, pre-selecting the default one
-            let paymentOptions = [];
-            for (const payment_option of orderToBePaid.payment_options) {
-                paymentOptions[paymentOptions.length] = payment_option.type;
-            }
-
-            if (paymentOptions.includes('ln')) {
-                paymentOptionSelected = 'ln';
-            } else if (paymentOptions.includes('lnurl')) {
-                paymentOptionSelected = 'lnurl';
-            } else if (paymentOptions.includes('btc')) {
-                paymentOptionSelected = 'btc';
-            }
-        }
-
-        let paymentOptionInfo = getCurrentPaymentOptionInfo();
-
-        let orderId = orderToBePaid.id;
-        let data = paymentOptionInfo.link;
-        let amount = paymentOptionInfo.amount_sats;
-
-        if (paymentOptionSelected === 'ln') {
-            paymentInfo = {
-                link: data,
-                protocol: 'lightning',
-                amount: amount,
-            }
-        } else if (paymentOptionSelected === 'lnurl') {
-            let lnAddressArray = data.split("@");
-            let name = lnAddressArray[0];
-            let domain = lnAddressArray[1];
-
-            let link = 'https://' + domain + '/.well-known/lnurlp/' + name;
-
-            paymentInfo = {
-                link: bech32Encode(link).toUpperCase(),
-                protocol: 'lightning',
-                amount: amount,
-            }
-        } else if (paymentOptionSelected === 'btc') {
-            let btcLink = data;
-
-            if (amount !== null || orderId !== null) {
-                btcLink += '?';
-            }
-            if (amount !== null) {
-                btcLink += 'amount=' + Number(amount / 100000000).toFixed(8);
-            }
-            if (orderId) {
-                if (btcLink.includes('amount')) {
-                    btcLink += '&';
-                }
-                btcLink += 'message=Order#' + orderId;
-            }
-
-            paymentInfo = {
-                link: btcLink,
-                protocol: 'bitcoin',
-                amount: amount,
-            }
-        } else {
-            alert('Payment type not supported: ' + type);
-        }
-
-        paymentModalVisible = true;
     }
 
     afterNavigate(async () => {
@@ -327,7 +239,7 @@
                                                 <p class="mb-2">{getAmountToPay(order)} sats</p>
                                             {/if}
 
-                                            <button class="btn btn-outline gap-2 mb-4 md:mb-2 h-16 md:h-12" on:click|preventDefault={() => {orderToBePaid = order; showPaymentDetails()}}>
+                                            <button class="btn btn-outline gap-2 mb-4 md:mb-2 h-16 md:h-12" on:click|preventDefault={() => {orderToBePaid = order; paymentModalVisible = true;}}>
                                                 <span class="h-7 w-7" ><Bitcoin /></span><p class="-ml-5 text-2xl">⚡</p> Pay order
                                             </button>
 
@@ -403,74 +315,24 @@
 {/if}
 
 <!-- QR payment modal -->
-<input type="checkbox" id="nostrTextConfirmation" class="modal-toggle" bind:checked={paymentModalVisible} on:change={() => showAutomaticPayments = false}/>
-<div class="modal">
-    <div class="modal-box relative bg-white">
-        <label for="nostrTextConfirmation" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+{#if orderToBePaid}
+    <input type="checkbox" id="nostrTextConfirmation" class="modal-toggle" bind:checked={paymentModalVisible} on:change={() => showAutomaticPayments = false}/>
+    <div class="modal">
+        <div class="modal-box relative bg-white">
+            <label for="nostrTextConfirmation" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
 
-        <h3 class="text-lg font-bold mb-0 text-black">
-            {#if Object.entries(ordersToBePaidNow).length === 1}
-                Pay your order:
-            {:else if Object.entries(ordersToBePaidNow).length > 1}
-                You have to pay {Object.entries(ordersToBePaidNow).length} orders. Start by paying this one:
-            {/if}
-        </h3>
-
-        {#if orderToBePaid && orderToBePaid.payment_options}
-            <div class="mt-4 md:mt-6 pb-0 flex flex-col justify-center items-center">
-                Payment type: <select bind:value={paymentOptionSelected} class="select select-bordered select-sm text-xs md:text-sm max-w-lg md:ml-1 { orderToBePaid.payment_options.length > 1 ? 'select-error border-2' : 'select-bordered' }" on:change="{() => showPaymentDetails()}">
-                {#each orderToBePaid.payment_options as payment_option}
-                    {#if payment_option.type === 'ln'}
-                        <option value="{payment_option.type}" selected>Lightning (invoice)</option>
-                    {:else if payment_option.type === 'lnurl'}
-                        <option value="{payment_option.type}" selected>Lightning (lnurl)</option>
-                    {:else if payment_option.type === 'btc'}
-                        <option value="{payment_option.type}">Bitcoin (on-chain)</option>
-                    {:else if payment_option.type === 'url'}
-                        <option value="{payment_option.type}">External payment method</option>
-                    {/if}
-                {/each}
-            </select>
-            </div>
-        {/if}
-
-        {#if paymentOptionSelected === 'btc'}
-            <div class="flex flex-col mt-3 justify-center items-center">
-                <div class="h-7 w-7" ><Bitcoin /></div>
-            </div>
-        {:else if ['ln', 'lnurl'].includes(paymentOptionSelected)}
-            <div class="flex flex-col mt-3 justify-center items-center">
-                <p class="text-2xl">⚡</p>
-            </div>
-        {/if}
-
-        {#if paymentInfo.link}
-            <div class="py-4 bg-white">
-                <QRLocal {paymentInfo} />
-
-                {#if paymentInfo.protocol === 'lightning' && paymentOptionSelected === 'lnurl'}
-                    <div class="mt-4 md:mt-6 pb-0 flex flex-col justify-center items-center">
-                        <div class="block justify-center items-center mx-auto mb-4">
-                            <button class="btn btn-success" on:click|preventDefault={() => paymentConfirmationModalVisible = true}>Mark as paid</button>
-                        </div>
-
-                        <ul class="list-disc list-outside text-xs md:text-sm">
-                            {#if paymentInfo.amount}
-                                <li class="mb-3">You must send <b>{paymentInfo.amount} sats</b> to the seller</li>
-                            {/if}
-                            <li class="mb-3 md:hidden">You can <b>tap the QR code</b> to open your Lightning wallet.</li>
-                            <li class="mb-3">Mark the payment as <b>paid</b> using the green button when you're done.</li>
-                            <li>As the <b>payment goes directly to the seller</b> and <b>Lightning payments are private</b>,
-                                we cannot tell you if the payment was successful. You must rely on your own wallet for this.</li>
-                        </ul>
-                    </div>
+            <h3 class="text-lg font-bold mb-0 text-black">
+                {#if Object.entries(ordersToBePaidNow).length === 1}
+                    Pay your order:
+                {:else if Object.entries(ordersToBePaidNow).length > 1}
+                    You have to pay {Object.entries(ordersToBePaidNow).length} orders. Start by paying this one:
                 {/if}
-            </div>
-        {:else}
-            <p>Error: payment address not available. Contact the seller.</p>
-        {/if}
+            </h3>
+
+            <PaymentWidget {orderToBePaid} bind:showPaymentDetails={showPaymentDetails} />
+        </div>
     </div>
-</div>
+{/if}
 
 <!-- Order paid confirmation -->
 <input type="checkbox" id="paymentConfirmation" class="modal-toggle" bind:checked={paymentConfirmationModalVisible} on:change={() => showAutomaticPayments = false}/>
