46f67cfe3aff70819cb85b0e46a60d3e60bda8a6
Ioan Bizău <i@ibz.me>
2023-10-12 17:31:45 +0300

    Move file selector to the left.

--- web/backoffice/src/lib/components/MediaEditor.svelte
+++ web/backoffice/src/lib/components/MediaEditor.svelte
@@ -20,12 +20,6 @@
 
 <div class="form-control mr-2 mt-4 w-full flex flex-row">
     <div class="w-1/2">
-        {#if item.media.length !== 0 || item.added_media.length !== 0}
-            <h3 class="text-xl">Media</h3>
-            <Gallery photos={item.media.concat(item.added_media)} />
-        {/if}
-    </div>
-    <div class="w-1/2">
         {#if item.media.length === 0 && item.added_media.length === 0}
             <p class="text-xl">Add some pictures...</p>
         {/if}
@@ -33,6 +27,12 @@
             <div class="w-full mt-4">
                 <input type="file" accept=".jpg, .jpeg, .png" class="file-input file-input-lg file-input-primary w-full max-w-xs" on:change={onFileSelected} bind:this={fileinput} />
             </div>
+        {/if}
+    </div>
+    <div class="w-1/2">
+        {#if item.media.length !== 0 || item.added_media.length !== 0}
+            <h3 class="text-xl">Media</h3>
+            <Gallery photos={item.media.concat(item.added_media)} />
         {/if}
     </div>
 </div>
