0bec0d3e7a95ae2a2071f09f2b9fdc356824c46e
Ioan Bizău <i@ibz.me>
2023-11-10 14:47:31 +0200

    Use "docker compose" instead of "docker-compose".

--- scripts/do.sh
+++ scripts/do.sh
@@ -6,13 +6,13 @@ ENV=$1
 shift
 
 if [ "$ENV" = "test" ]; then
-    docker-compose -f docker-compose.yml -f docker-compose.dev.yml -f docker-compose.test.yml $@
+    docker compose -f docker-compose.yml -f docker-compose.dev.yml -f docker-compose.test.yml $@
 elif [ "$ENV" = "dev" ]; then
-    docker-compose -f docker-compose.yml -f docker-compose.dev.yml $@
+    docker compose -f docker-compose.yml -f docker-compose.dev.yml $@
 elif [ "$ENV" = "staging" ]; then
-    docker-compose -f docker-compose.yml -f docker-compose.staging.yml $@
+    docker compose -f docker-compose.yml -f docker-compose.staging.yml $@
 elif [ "$ENV" = "prod" ]; then
-    docker-compose -f docker-compose.yml -f docker-compose.staging.yml -f docker-compose.prod.yml $@
+    docker compose -f docker-compose.yml -f docker-compose.staging.yml -f docker-compose.prod.yml $@
 else
     echo $USAGE
     exit 1
