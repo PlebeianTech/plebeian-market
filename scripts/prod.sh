c6deaf7930b6eb91de517325d23b37f01131796c
Luis Miguel <ktecho@ktecho.com>
2023-11-13 13:36:27 +0100

    Preparing scripts + docker-compose to be able to deploy to prod (#362)

--- scripts/prod.sh
+++ scripts/prod.sh
@@ -2,4 +2,4 @@
 
 DIR="$(cd "$(dirname "$0")" && pwd)"
 
-$DIR/do.sh prod build && $DIR/do.sh prod down && $DIR/do.sh prod up -d
+$DIR/do.sh prod build && $DIR/do.sh prod down && docker volume rm plebeian-market_buyer-app-static-content && $DIR/do.sh prod up -d
