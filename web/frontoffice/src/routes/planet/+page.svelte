2e438ac909827d415d4ba6aa4e8429c25de84ad9
ktecho <ktecho@ktecho.com>
2023-11-16 13:30:28 +0100

    Create the Planet page

---
+++ web/frontoffice/src/routes/planet/+page.svelte
@@ -0,0 +1,13 @@
+<svelte:head>
+    <title>Plebeian Market</title>
+</svelte:head>
+
+<script lang="ts">
+    import Titleh1 from "$sharedLib/components/layout/Title-h1.svelte";
+    import ProductCardBrowser from "$lib/components/stores/ProductCardBrowser.svelte";
+    export let maxProductsLoaded: number = 0;   // Unlimited
+</script>
+
+<Titleh1>Planet</Titleh1>
+
+<ProductCardBrowser {maxProductsLoaded} />
