e7bfe8dd3c616d2e470865233ec89a602436a254
Luis Miguel <ktecho@ktecho.com>
2023-11-16 10:47:11 +0100

    Fix Private Messages bubble colors

--- web/frontoffice/src/lib/components/nostr/SimpleNote.svelte
+++ web/frontoffice/src/lib/components/nostr/SimpleNote.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div class="chat mt-0 md:mt-4" class:chat-start={!message.my_message_replying_to_this_pubkey} class:chat-end={message.my_message_replying_to_this_pubkey}>
-    <div class="chat-bubble max-w-[80%] lg:max-w-[60%]" class:chat-bubble-primary={!message.my_message_replying_to_this_pubkey} class:chat-bubble-secondary={message.my_message_replying_to_this_pubkey}>
+    <div class="chat-bubble max-w-[80%] lg:max-w-[60%] text-white" class:chat-bubble-primary={!message.my_message_replying_to_this_pubkey} class:chat-bubble-accent={message.my_message_replying_to_this_pubkey}>
         {@html message.message}
         <div class="space-y-1 text-xs opacity-60 text-right mt-1">{formatTimestamp(message.created_at)}</div>
     </div>
