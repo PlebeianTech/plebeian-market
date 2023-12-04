16d144d26513f4dfb378a5ef0b29f36652ec9776
ktecho <ktecho@ktecho.com>
2023-12-04 12:11:35 +0100

    Fix product slider UI

--- web/frontoffice/src/lib/components/pagebuilder/SectionsProductsSlider.svelte
+++ web/frontoffice/src/lib/components/pagebuilder/SectionsProductsSlider.svelte
@@ -22,8 +22,6 @@
     let products: {[productId: string]: {}} = {};
     let productsLoaded = false;
 
-    let maxHeightProductSlider = "32rem";
-
     onMount(async () => {
         let productIDs = getItemsFromSection(pageId, sectionId, 'products');
 
@@ -99,26 +97,26 @@
     {/if}
 
     <div id="carouselDarkVariant"
-         class="relative" class:hidden={!productsLoaded}
+         class="w-full" class:hidden={!productsLoaded}
          data-te-carousel-init
          data-te-ride="carousel">
 
         <!-- Carousel items -->
-        <div class="w-full max-h-[{maxHeightProductSlider}] overflow-hidden">
+        <div class="w-full h-[36rem] overflow-hidden">
             {#each Object.entries(products) as [_, product], i}
                 {#if product.event.kind === EVENT_KIND_PRODUCT || (product.event.kind === EVENT_KIND_AUCTION && product.ended === false)}
-                    <div class="w-full max-h-[{maxHeightProductSlider}] overflow-hidden block bg-base-200 rounded-xl !transform-none opacity-0 transition-opacity duration-[600ms] ease-in-out motion-reduce:transition-none"
+                    <div class="h-[36rem] max-h-full w-full relative float-left -mr-[100%] bg-base-200/40 rounded-xl !transform-none opacity-0 transition-opacity duration-[600ms] ease-in-out motion-reduce:transition-none"
                          class:hidden={i > 0}
                          data-te-carousel-fade
                          data-te-carousel-item
                          data-te-carousel-active={i === 0 ? true : null}>
-                        <div class="w-full max-h-[{maxHeightProductSlider}] overflow-hidden block md:flex">
-                            <div class="w-full md:w-6/12 max-h-[{maxHeightProductSlider}] overflow-hidden">
+                        <div class="relative h-full w-auto md:flex overflow-hidden">
+                            <div class="h-full w-full md:w-6/12 overflow-hidden">
                                 <img class="h-full w-auto mx-auto p-4 md:p-6" alt="{product.name ?? 'Product #' + i}"
                                      src="{product.images ? product.images[0] : product.image ?? productImageFallback}"/>
                             </div>
 
-                            <div class="w-full md:w-6/12 max-h-[{maxHeightProductSlider}] overflow-hidden p-4 md:p-16 md:pl-12 md:text-lg">
+                            <div class="w-full md:w-6/12 overflow-hidden p-4 md:p-16 md:pl-12 md:text-lg">
                                 {#if product.markdownText}
                                     <div class="z-[300] prose lg:prose-xl">
                                         <SvelteMarkdown source={product.markdownText} />
@@ -152,7 +150,6 @@
                   data-te-target="#carouselDarkVariant"
                   data-te-slide="prev"><span style="transform: scale(-1);">&#x279c;</span></span>
         </div>
-
         <!-- Carousel controls - next item-->
         <div class="absolute bottom-0 right-0 top-0 z-[1] flex w-[3%] items-center justify-center">
             <span class="absolute -ml-6 bg-white rounded-full shadow-gray-500 shadow-md hover:shadow-lg h-12 w-12 text-3xl flex items-center justify-center
@@ -164,7 +161,7 @@
                   data-te-slide="next">&#x279c;</span>
         </div>
 
-        <div class="absolute inset-x-0 mt-6 z-[2] mx-[15%] mb-4 flex list-none justify-center p-0" data-te-carousel-indicators>
+        <div class="inset-x-0 mt-6 z-[2] mx-[15%] mb-4 flex list-none justify-center p-0" data-te-carousel-indicators>
             {#each Object.entries(products) as [_, product], i}
                 {#if product.event.kind === EVENT_KIND_PRODUCT || (product.event.kind === EVENT_KIND_AUCTION && product.ended === false )}
                     <button data-te-target="#carouselDarkVariant"
