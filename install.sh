482909127dab1436edc7f10917e0dcc1a8946b65
Ioan Bizău <i@ibz.me>
2024-01-17 17:12:21 +0200

    This is currently run as root.

--- install.sh
+++ install.sh
@@ -4,22 +4,20 @@ docker -v
 if [ $? -ne 0 ]; then
     echo "Docker not installed. Installing..."
 
-    sudo apt-get update
-    sudo apt-get -y install ca-certificates curl gnupg
-    sudo install -m 0755 -d /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-    sudo chmod a+r /etc/apt/keyrings/docker.gpg
+    apt-get update
+    apt-get -y install ca-certificates curl gnupg
+    install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    chmod a+r /etc/apt/keyrings/docker.gpg
     echo \
         "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
         $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-        sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt-get update
-    sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-    sudo usermod -aG docker $USER
-    newgrp docker
+        tee /etc/apt/sources.list.d/docker.list > /dev/null
+    apt-get update
+    apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 fi
 
-sudo apt-get -y install git
+apt-get -y install git
 
 echo "------------------------"
 
