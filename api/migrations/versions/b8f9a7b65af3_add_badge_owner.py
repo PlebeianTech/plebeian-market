f5c2e935b4b1cd16d51b529de9e98ec33dc27a88
Ioan Bizău <i@ibz.me>
2024-01-24 17:44:27 +0200

    owner

--- api/migrations/versions/b8f9a7b65af3_add_badge_owner.py
+++ api/migrations/versions/b8f9a7b65af3_add_badge_owner.py
@@ -16,7 +16,7 @@ depends_on = None
 
 def upgrade():
     with op.batch_alter_table('badges', schema=None) as batch_op:
-        batch_op.add_column(sa.Column('owner_public_key', sa.String(length=64), nullable=False, server_default="28c62ad00fe085a7b2b54a05fdf333367e3e6a4d9650ad1c159f964705b76928"))
+        batch_op.add_column(sa.Column('owner_public_key', sa.String(length=64), nullable=False, server_default="76cc29acb8008c68b105cf655d34de0b1f7bc0215eaae6bbc83173d6d3f7b987"))
         batch_op.alter_column('nostr_event_id',
                existing_type=sa.VARCHAR(length=64),
                nullable=True)
