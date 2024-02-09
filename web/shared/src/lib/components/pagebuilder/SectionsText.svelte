056c4bee49c1a5f5e388ea4f80ec440fbf71cbe1
ktecho <ktecho@ktecho.com>
2024-02-09 18:12:44 +0100

    Use lexical converted to html instead of the editor widget

--- web/shared/src/lib/components/pagebuilder/SectionsText.svelte
+++ web/shared/src/lib/components/pagebuilder/SectionsText.svelte
@@ -2,17 +2,16 @@
     import {onMount} from "svelte";
     import {subscribeConfiguration, getConfigurationKey} from "$sharedLib/services/nostr";
     import {fileConfiguration} from "$sharedLib/stores";
-    import RichTextComposer from "$sharedLib/components/pagebuilder/lexical-editor/RichTextComposer.svelte";
-    //import {transformLexicalJSONToHTML} from "@bowline/svelte-lexical/src/transform-json-to-html";
-    // import {getHtmlFromLexicalJSON} from "$sharedLib/components/pagebuilder/lexical-editor/transform-json-to-html";
-    //import {fromArrayPack, unminify} from "lexical-minifier";
+    import {transformLexicalJSONToHTML} from "@bowline/svelte-lexical/src/transform-json-to-html";
+    //import {getHtmlFromLexicalJSON} from "$sharedLib/components/pagebuilder/lexical-editor/transform-json-to-html";
+    import {fromArrayPack, unminify} from "lexical-minifier";
     import '$sharedLib/components/pagebuilder/lexical-editor/lexical.css';
 
     export let pageId;
     export let sectionId;
 
     let initialMinifiedLexicalContent: string | null = null;
-//    let lexicalHtml: string | null = null;
+    let lexicalHtml: string | null = null;
 
     onMount(() => {
         if ($fileConfiguration?.admin_pubkeys?.length > 0) {
@@ -24,10 +23,10 @@
                         receivedAt = rcAt;
                         initialMinifiedLexicalContent = initialMinifiedLexicalContentFromNostr;
 
-                        // let jsonParsed = JSON.parse(initialMinifiedLexicalContent);
-                        // let arrayUnpacked = fromArrayPack(jsonParsed);
-                        // let unminified = unminify(arrayUnpacked) ?? [];
-                        // lexicalHtml = transformLexicalJSONToHTML(JSON.stringify({ root: unminified } ?? []));
+                        let jsonParsed = JSON.parse(initialMinifiedLexicalContent);
+                        let arrayUnpacked = fromArrayPack(jsonParsed);
+                        let unminified = unminify(arrayUnpacked) ?? [];
+                        lexicalHtml = transformLexicalJSONToHTML(JSON.stringify({ root: unminified } ?? []));
                     }
                 });
         }
@@ -40,13 +39,8 @@
             <span class="loading loading-bars w-24"></span>
         </div>
     {:else}
-        <div class="z-[300]">
-            <RichTextComposer {initialMinifiedLexicalContent} editable={false} />
-        </div>
-        <!--
         <div class="z-[300] lexical-div aaprose w-full lg:11/12 xl:w-9/12 2xl:w-7/12 mx-auto">
             {@html lexicalHtml}
         </div>
-        -->
     {/if}
 </main>
