5a2469a2caa75f29c3ec3f394d710b93513c37f0
ktecho <ktecho@ktecho.com>
2024-02-27 15:45:10 +0100

    Buyer Settings is now different menu entries

---
+++ web/frontoffice/src/routes/verification/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import Titleh1 from "$sharedLib/components/layout/Title-h1.svelte";
+    import ExternalIdentities from "$lib/components/settings/ExternalIdentities.svelte";
+</script>
+
+<svelte:head>
+    <title>Get Verified</title>
+</svelte:head>
+
+<Titleh1>Get Verified</Titleh1>
+
+<ExternalIdentities />
